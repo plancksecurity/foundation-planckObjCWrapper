d301347aa5612911907e6fbd9d9d1233138f9295
Edouard Tisserant <devnull@localhost>
2015-11-06 22:38:43 +0100

    More testing. Now with a reliable color test.

--- iOSTests/iOSTests.m
+++ iOSTests/iOSTests.m
@@ -11,6 +11,8 @@
 #import "pEpiOSAdapter/PEPiOSAdapter.h"
 #import "pEpiOSAdapter/PEPSession.h"
 
+static BOOL first = YES;
+
 @interface iOSTests : XCTestCase
 
 @end
@@ -19,8 +21,30 @@
 
 PEPSession *session;
 
+-(void)delFile : (NSString *)path {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *error;
+    BOOL fileExists = [fileManager fileExistsAtPath:path];
+    NSLog(@"Path to file: %@", path);
+    NSLog(@"File exists: %d", fileExists);
+    NSLog(@"Is deletable file at path: %d", [fileManager isDeletableFileAtPath:path]);
+    if (fileExists)
+    {
+        BOOL success = [fileManager removeItemAtPath:path error:&error];
+        if (!success) NSLog(@"Error: %@", [error localizedDescription]);
+    }
+}
+
 - (void)setUp {
     [super setUp];
+    if(first){
+        NSString* home = [[[NSProcessInfo processInfo]environment]objectForKey:@"HOME"];
+        
+        [self delFile:[home stringByAppendingPathComponent:@".pEp_management.db"]];
+        [self delFile:[home stringByAppendingPathComponent:@".gnupg"]];
+        
+        first = NO;
+    }
     [PEPiOSAdapter setupTrustWordsDB:[NSBundle bundleForClass:[self class]]];
     session = [[PEPSession alloc]init];
     XCTAssert(session);
@@ -48,27 +72,19 @@ PEPSession *session;
     
 }
 
-- (void)testMCOMessageAPI {
-    NSArray *keyFiles = @[
-                          // Our test user :
-                          // pEp Test Alice (test key don't use) <pep.test.alice@pep-project.org>
-                          // 4ABE3AAF59AC32CFE4F86500A9411D176FF00E97
-                          @"6FF00E97_sec.asc",
-                          // Other peers :
-                          // pEp Test Bob (test key, don't use) <pep.test.bob@pep-project.org>
-                          // BFCDB7F301DEEEBBF947F29659BFF488C9C2EE39
-                          @"0xC9C2EE39.asc",
-                          // pEp Test John (test key, don't use) <pep.test.john@pep-project.org>
-                          // AA2E4BEB93E5FE33DEFD8BE1135CD6D170DCF575
-                          @"0x70DCF575.asc"];
-    //NSMutableDictionary *keysStrings = [[NSMutableDictionary alloc] init];
+- (void)importBundledKey : (NSString*)item {
+    
+    NSString *txtFilePath = [[[NSBundle bundleForClass:[self class]] resourcePath] stringByAppendingPathComponent:item];
+    NSString *txtFileContents = [NSString stringWithContentsOfFile:txtFilePath encoding:NSUTF8StringEncoding error:NULL];
+    //[keysStrings setObject:txtFileContents forKey:txtFilePath];
+    [session importKey:txtFileContents];
+}
 
-    for (NSString *item in keyFiles) {
-        NSString *txtFilePath = [[[NSBundle bundleForClass:[self class]] resourcePath] stringByAppendingPathComponent:item];
-        NSString *txtFileContents = [NSString stringWithContentsOfFile:txtFilePath encoding:NSUTF8StringEncoding error:NULL];
-        //[keysStrings setObject:txtFileContents forKey:txtFilePath];
-        [session importKey:txtFileContents];
-    }
+- (void)testOutgoingColors {
+    // Our test user :
+    // pEp Test Alice (test key don't use) <pep.test.alice@pep-project.org>
+    // 4ABE3AAF59AC32CFE4F86500A9411D176FF00E97
+    [self importBundledKey:@"6FF00E97_sec.asc"];
     
     NSMutableDictionary *identAlice = [NSMutableDictionary dictionaryWithObjectsAndKeys:
                                   @"pEp Test Alice", @"username",
@@ -79,39 +95,71 @@ PEPSession *session;
  
     [session mySelf:identAlice];
     
-    NSMutableDictionary *identBob = [NSMutableDictionary dictionaryWithObjectsAndKeys:
-                                  @"pEp Test Bob", @"username",
-                                  @"pep.test.bob@pep-project.org", @"address",
-                                  @"42", @"user_id",
-                                  @"BFCDB7F301DEEEBBF947F29659BFF488C9C2EE39",@"fpr",
-                                  nil];
-    
-    [session updateIdentity:identBob];
-    
-    NSMutableDictionary *identJohn = [NSMutableDictionary dictionaryWithObjectsAndKeys:
-                                  @"pEp Test John", @"username",
-                                  @"pep.test.john@pep-project.org", @"address",
-                                  @"101", @"user_id",
-                                  @"AA2E4BEB93E5FE33DEFD8BE1135CD6D170DCF575",@"fpr",
-                                  nil];
-    
-    [session updateIdentity:identJohn];
-    
+    // Make a mail message, with (for now) unknown peer Bob
     MCOAddress * from = [MCOAddress addressWithDisplayName:@"pEp Test Alice" mailbox:@"pep.test.alice@pep-project.org"];
     MCOAddress * to1 = [MCOAddress addressWithDisplayName:@"pEp Test Bob" mailbox:@"pep.test.bob@pep-project.org"];
-    MCOAddress * to2 = [MCOAddress addressWithDisplayName:@"pEp Test John" mailbox:@"pep.test.John@pep-project.org"];
 
     MCOMessageBuilder * builder = [[MCOMessageBuilder alloc] init];
     [[builder header] setFrom:from];
     [[builder header] setTo:@[to1]];
-    [[builder header] setCc:@[to2]];
     [[builder header] setSubject:@"All Green Test"];
-    //[[builder header] setDate:[NSDate dateWithTimeIntervalSinceReferenceDate:0]];
-    //[[builder header] setMessageID:@"MyMessageID123@mail.gmail.com"];
     [builder setTextBody:@"This is a text content"];
     builder.outgoing = YES;
-    
 
-    XCTAssert([session outgoingMessageColor:builder]==PEP_rating_green);
+    // Test with unknown Bob
+    PEP_color clr = [session outgoingMessageColor:builder];
+    XCTAssert( clr == PEP_rating_unencrypted);
+
+    // Now let see with bob's pubkey already known
+    // pEp Test Bob (test key, don't use) <pep.test.bob@pep-project.org>
+    // BFCDB7F301DEEEBBF947F29659BFF488C9C2EE39
+    [self importBundledKey:@"0xC9C2EE39.asc"];
+    
+    NSMutableDictionary *identBob = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                     @"pEp Test Bob", @"username",
+                                     @"pep.test.bob@pep-project.org", @"address",
+                                     @"42", @"user_id",
+                                     @"BFCDB7F301DEEEBBF947F29659BFF488C9C2EE39",@"fpr",
+                                     nil];
+    
+    [session updateIdentity:identBob];
+
+    // Should be yellow, since no handshake happened.
+    clr = [session outgoingMessageColor:builder];
+    XCTAssert( clr == PEP_rating_yellow);
+    
+    // Let' say we got that handshake, set PEP_ct_confirmed in Bob's identity
+    [identBob setObject:[NSNumber
+                 numberWithInt:
+                         PEP_ct_confirmed | [[identBob objectForKey:@"comm_type"] intValue]]
+                 forKey:@"comm_type"];
+
+    [session updateIdentity:identBob];
+
+    // This time it should be green
+    clr = [session outgoingMessageColor:builder];
+    XCTAssert( clr == PEP_rating_green);
+
+    // Now let see if it turns back yellow if we add an unconfirmed folk.
+    // pEp Test John (test key, don't use) <pep.test.john@pep-project.org>
+    // AA2E4BEB93E5FE33DEFD8BE1135CD6D170DCF575
+    [self importBundledKey:@"0x70DCF575.asc"];
+    
+    NSMutableDictionary *identJohn = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                      @"pEp Test John", @"username",
+                                      @"pep.test.john@pep-project.org", @"address",
+                                      @"101", @"user_id",
+                                      @"AA2E4BEB93E5FE33DEFD8BE1135CD6D170DCF575",@"fpr",
+                                      nil];
+    
+    [session updateIdentity:identJohn];
+    
+    MCOAddress * to2 = [MCOAddress addressWithDisplayName:@"pEp Test John" mailbox:@"pep.test.john@pep-project.org"];
+    [[builder header] setCc:@[to2]];
+
+    // Yellow ?
+    clr = [session outgoingMessageColor:builder];
+    XCTAssert( clr == PEP_rating_yellow);
+
 }
 @end
