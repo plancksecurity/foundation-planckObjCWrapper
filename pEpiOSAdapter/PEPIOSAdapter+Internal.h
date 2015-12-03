2239fd764c8d81b37a1965002614d77152c1198c
Edouard Tisserant <devnull@localhost>
2015-12-03 20:00:37 +0100

    Added forgotten header.

---
+++ pEpiOSAdapter/PEPIOSAdapter+Internal.h
@@ -0,0 +1,23 @@
+//
+//  PEPIOSAdapter+Internal.h
+//  pEpiOSAdapter
+//
+//  Created by Edouard Tisserant on 11/11/15.
+//  Copyright © 2015 p≡p. All rights reserved.
+//
+
+#ifndef PEPIOSAdapter_Internal_h
+#define PEPIOSAdapter_Internal_h
+
+#import "PEPQueue.h"
+
+@interface PEPiOSAdapter ()
+
+// this messages are for internal use only; do not call
+
++ (void)registerExamineFunction:(PEP_SESSION)session;
++ (PEPQueue*)getQueue;
+
+@end
+
+#endif /* PEPIOSAdapter_Internal_h */
