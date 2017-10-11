f298bdb7914b1cf7e036639feee459e4a21db2c9
buff <andreas@pep-project.org>
2017-10-11 10:03:26 +0200

    IOSAD-50 adds fake session class later given to clients

---
+++ pEpObjCAdapter/PEPSession.h
@@ -0,0 +1,13 @@
+//
+//  PEPSession.h
+//  pEpObjCAdapter
+//
+//  Created by Andreas Buff on 11.10.17.
+//  Copyright © 2017 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PEPSession : NSObject
+
+@end
---
+++ pEpObjCAdapter/PEPSession.m
@@ -0,0 +1,13 @@
+//
+//  PEPSession.m
+//  pEpObjCAdapter
+//
+//  Created by Andreas Buff on 11.10.17.
+//  Copyright © 2017 p≡p. All rights reserved.
+//
+
+#import "PEPSession.h"
+
+@implementation PEPSession
+
+@end
