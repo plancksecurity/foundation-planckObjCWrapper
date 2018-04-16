456281fff0eb3bfc00ed220bd9202985ee800f33
Dirk Zimmermann <dirk@pep-project.org>
2018-04-16 11:01:18 +0200

    IOSAD-91 new class: PEPAttachment

---
+++ pEpObjCAdapter/PEPAttachment.h
@@ -0,0 +1,13 @@
+//
+//  PEPAttachment.h
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 16.04.18.
+//  Copyright © 2018 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PEPAttachment : NSObject
+
+@end
---
+++ pEpObjCAdapter/PEPAttachment.m
@@ -0,0 +1,13 @@
+//
+//  PEPAttachment.m
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 16.04.18.
+//  Copyright © 2018 p≡p. All rights reserved.
+//
+
+#import "PEPAttachment.h"
+
+@implementation PEPAttachment
+
+@end
