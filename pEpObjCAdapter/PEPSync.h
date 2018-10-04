5baaf397e8711dca5a81087b4b8a2fbbbbe4731c
Dirk Zimmermann <dirk@pep-project.org>
2018-10-04 15:30:03 +0200

    IOSAD-103 PEPSync

---
+++ pEpObjCAdapter/PEPSync.h
@@ -0,0 +1,13 @@
+//
+//  PEPSync.h
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 04.10.18.
+//  Copyright © 2018 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PEPSync : NSObject
+
+@end
---
+++ pEpObjCAdapter/PEPSync.m
@@ -0,0 +1,13 @@
+//
+//  PEPSync.m
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 04.10.18.
+//  Copyright © 2018 p≡p. All rights reserved.
+//
+
+#import "PEPSync.h"
+
+@implementation PEPSync
+
+@end
