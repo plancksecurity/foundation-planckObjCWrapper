7142a96911410035eb20919d40e455ccddad6047
Dirk Zimmermann <dirk@pep-project.org>
2018-10-29 13:44:37 +0100

    IOSAD-103 SendMessageDelegate in own file

---
+++ pEpObjCTests/TestUtils/PEPSessionTestSendMessageDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PEPSessionTestSendMessageDelegate.h
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 29.10.18.
+//  Copyright © 2018 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPSyncSendMessageDelegate.h"
+
+@interface PEPSessionTestSendMessageDelegate : NSObject<PEPSyncSendMessageDelegate>
+
+@end
---
+++ pEpObjCTests/TestUtils/PEPSessionTestSendMessageDelegate.m
@@ -0,0 +1,13 @@
+//
+//  PEPSessionTestSendMessageDelegate.m
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 29.10.18.
+//  Copyright © 2018 p≡p. All rights reserved.
+//
+
+#import "PEPSessionTestSendMessageDelegate.h"
+
+@implementation PEPSessionTestSendMessageDelegate
+
+@end
