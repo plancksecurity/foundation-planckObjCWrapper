481a017070c4bbe500433fab2eb580bfaa2c98d6
Dirk Zimmermann <dz@pep.security>
2019-02-11 11:40:05 +0100

    IOS-1475 PEP_KEY_IMPORT_STATUS_UNKNOWN is not an error.

--- pEpObjCAdapter/NSError+PEP.m
+++ pEpObjCAdapter/NSError+PEP.m
@@ -20,6 +20,7 @@ static NSString *s_pEpAdapterDomain = @"security.pEp.ObjCAdapter";
         case PEP_DECRYPTED:
         case PEP_UNENCRYPTED:
         case PEP_DECRYPT_NO_KEY:
+        case PEP_KEY_IMPORT_STATUS_UNKNOWN:
             return nil;
             break;
 
