5f9504a7fadaa6edbf531f500481c7f6e8320341
Dirk Zimmermann <dz@pep.security>
2019-02-20 09:19:12 +0100

    IOS-1480 Create adapter framework.

---
+++ pEpObjCAdapterFramework/pEpObjCAdapterFramework.h
@@ -0,0 +1,19 @@
+//
+//  pEpObjCAdapterFramework.h
+//  pEpObjCAdapterFramework
+//
+//  Created by Dirk Zimmermann on 20.02.19.
+//  Copyright © 2019 p≡p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for pEpObjCAdapterFramework.
+FOUNDATION_EXPORT double pEpObjCAdapterFrameworkVersionNumber;
+
+//! Project version string for pEpObjCAdapterFramework.
+FOUNDATION_EXPORT const unsigned char pEpObjCAdapterFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pEpObjCAdapterFramework/PublicHeader.h>
+
+
