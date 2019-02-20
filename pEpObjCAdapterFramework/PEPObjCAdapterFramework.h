ba391fd3dd1daf5be3293601b0ab12c6a4a83ca4
Dirk Zimmermann <dz@pep.security>
2019-02-20 13:32:28 +0100

    IOS-1480 Create correctly named adapter framework.

---
+++ pEpObjCAdapterFramework/PEPObjCAdapterFramework.h
@@ -0,0 +1,19 @@
+//
+//  PEPObjCAdapterFramework.h
+//  PEPObjCAdapterFramework
+//
+//  Created by Dirk Zimmermann on 20.02.19.
+//  Copyright © 2019 p≡p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PEPObjCAdapterFramework.
+FOUNDATION_EXPORT double PEPObjCAdapterFrameworkVersionNumber;
+
+//! Project version string for PEPObjCAdapterFramework.
+FOUNDATION_EXPORT const unsigned char PEPObjCAdapterFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PEPObjCAdapterFramework/PublicHeader.h>
+
+
