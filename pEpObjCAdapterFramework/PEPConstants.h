ce37d6ec0ada558da9f66b6183a6b23c043acb61
Dirk Zimmermann <dz@pep.security>
2019-02-27 16:08:36 +0100

    IOS-1480 All the good ones were already taken.

--- pEpObjCAdapterFramework/PEPConstants.h
+++ pEpObjCAdapterFramework/PEPConstants.h
@@ -9,7 +9,7 @@
 #ifndef PEPConstants_h
 #define PEPConstants_h
 
-typedef enum _PEP_decrypt_flags {
+typedef enum _ObjC_PEP_decrypt_flags {
     PEPDecryptFlagOwnPrivateKey = 0x1, // PEP_decrypt_flag_own_private_key = 0x1,
     PEPDecryptFlagConsume = 0x2, // PEP_decrypt_flag_consume = 0x2,
     PEPDecryptFlagIgnore = 0x4, // PEP_decrypt_flag_ignore = 0x4,
@@ -18,7 +18,7 @@ typedef enum _PEP_decrypt_flags {
     PEPDecryptFlagUntrustedServer = 0x100 // PEP_decrypt_flag_untrusted_server = 0x100
 } PEPDecryptFlags; // PEP_decrypt_flags;
 
-typedef enum _PEP_enc_format {
+typedef enum _ObjC_PEP_enc_format {
     PEPEncNone = 0, // PEP_enc_none = 0, // message is not encrypted
     PEPEncPieces, // PEP_enc_pieces, // inline PGP + PGP extensions
     PEPEncSMIME, // PEP_enc_S_MIME, // RFC5751
@@ -27,7 +27,7 @@ typedef enum _PEP_enc_format {
     PEPEncPGPMIMEOutlook1 // PEP_enc_PGP_MIME_Outlook1 // Message B0rken by Outlook type 1
 } PEPEncFormat;
 
-typedef enum _PEP_rating {
+typedef enum _ObjC_PEP_rating {
     PEPRatingUndefined = 0,// PEP_rating_undefined = 0,
     PEPRatingCannotDecrypt, // PEP_rating_cannot_decrypt,
     PEPRatingHaveNoKey, // PEP_rating_have_no_key,
