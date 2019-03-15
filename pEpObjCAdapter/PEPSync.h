5dd5f35f0314c595426205d086309ddd685f7ff9
Dirk Zimmermann <dz@pep.security>
2019-03-15 16:14:50 +0100

    IOS-1475 Re-add PEPSync.

---
+++ pEpObjCAdapter/PEPSync.h
@@ -0,0 +1,43 @@
+//
+//  PEPSync.h
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 04.10.18.
+//  Copyright © 2018 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "pEpEngine.h"
+
+#import "PEPSendMessageDelegate.h"
+#import "PEPNotifyHandshakeDelegate.h"
+
+@class PEPSyncSendMessageDelegate;
+
+/**
+ @see libpEpAdapter: Adapter.{cc|hh}
+ @see sync_codec.h
+ */
+@interface PEPSync : NSObject
+
+/**
+ Creates an engine session.
+ */
++ (PEP_SESSION)createSession:(NSError **)error;
+
+/**
+ Releases an engine session that was created by `createSession`.
+ */
++ (void)releaseSession:(PEP_SESSION)session;
+
+- (instancetype)initWithSendMessageDelegate:(id<PEPSendMessageDelegate>
+                                             _Nonnull)sendMessageDelegate
+                    notifyHandshakeDelegate:(id<PEPNotifyHandshakeDelegate>
+                                             _Nonnull)notifyHandshakeDelegate;
+
+- (void)startup;
+
+- (void)shutdown;
+
+@end
