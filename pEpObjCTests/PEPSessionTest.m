6894a8c6594df5254ddb45f74d7351b0dadae47a
Dirk Zimmermann <dz@pep.security>
2019-05-27 17:38:32 +0200

    Backed out: Only start sync when needed and ready.

--- pEpObjCTests/PEPSessionTest.m
+++ pEpObjCTests/PEPSessionTest.m
@@ -36,6 +36,8 @@
 
     [self pEpCleanUp];
 
+    [self startSync];
+
     [PEPObjCAdapter setUnEncryptedSubjectEnabled:NO];
 }
 
@@ -1311,7 +1313,7 @@
     XCTAssertTrue([session mySelf:identMe error:&error]);
     XCTAssertNil(error);
 
-    [self startSync];
+    [self reStartSync];
 
     XCTKVOExpectation *expHaveMessage = [[XCTKVOExpectation alloc]
                                          initWithKeyPath:@"lastMessage"
