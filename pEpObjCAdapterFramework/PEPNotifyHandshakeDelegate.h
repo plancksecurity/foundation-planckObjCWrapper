218ca625d6073f28fd073c1469d440a8e779b5d6
Dirk Zimmermann <dz@pep.security>
2019-08-01 12:38:54 +0200

    IOSAD-114 Docs for PEPNotifyHandshakeDelegate.

--- pEpObjCAdapterFramework/PEPNotifyHandshakeDelegate.h
+++ pEpObjCAdapterFramework/PEPNotifyHandshakeDelegate.h
@@ -12,8 +12,29 @@
 
 @class PEPIdentity;
 
+/**
+ Handles notifications from the engine to the app that involve UI.
+ */
 @protocol PEPNotifyHandshakeDelegate <NSObject>
 
+/**
+ Requests the app to show a handshake dialog, or change the icon that represents
+ the key-sync state (as in, grouped, or sole, etc.).
+
+ After the dialog has been shown, the user's choices can be communicated back to the engine
+ via `PEPSessionProtocol.deliverHandshakeResult`.
+
+ @param object This can be used to thread information from the app through the sync-loop back to
+               the app. Currently unused and always nil.
+ @param me The own identity.
+           Note that in some cases, only the most essential properties are set.
+ @param partner The partner identity.
+                Note that in some cases, only the most essential properties are set.
+ @param signal The kind of action that is happening or requested.
+ @return A status indicating errors in the immediate/synchronous handling of the call.
+         The (delayed) response from the user are communicated to the engine
+         via separate method calls.
+ */
 - (PEPStatus)notifyHandshake:(void * _Nullable)object
                           me:(PEPIdentity * _Nonnull)me
                      partner:(PEPIdentity * _Nonnull)partner
