c4cafcddb2339466aa97b36dcf54d75e925abbfa
Dirk Zimmermann <dz@pep.security>
2020-06-25 17:08:21 +0200

    IOSAD-172 Dummy implementation

--- pEpObjCAdapter/PEPPasswordCache.m
+++ pEpObjCAdapter/PEPPasswordCache.m
@@ -10,4 +10,29 @@
 
 @implementation PEPPasswordCache
 
+/// Internal constructor (for now).
+- (instancetype)initTimeout:(NSUInteger)timeout
+{
+    self = [super init];
+    if (self) {
+        _timeout = timeout;
+    }
+    return self;
+}
+
+/// Public constructor with default values.
+- (instancetype)init
+{
+    return [self initTimeout:10 * 60];
+}
+
+- (void)addPassword:(NSString *)password
+{
+}
+
+- (NSArray *)passwords
+{
+    return @[];
+}
+
 @end
