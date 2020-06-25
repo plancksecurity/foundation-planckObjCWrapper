d968550763fcb24fcdffa78c1ecf2993a7b7949a
Dirk Zimmermann <dz@pep.security>
2020-06-25 17:08:21 +0200

    IOSAD-172 Interface for password cache

--- pEpObjCAdapter/PEPPasswordCache.h
+++ pEpObjCAdapter/PEPPasswordCache.h
@@ -12,6 +12,15 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface PEPPasswordCache : NSObject
 
+/// Timeout of passwords in seconds, the default being 10 minutes.
+@property (nonatomic) NSUInteger timeout;
+
+/// Add a new password.
+- (void)addPassword:(NSString *)password;
+
+/// Retrieve the current list of cached passwords, including the empty one.
+- (NSArray *)passwords;
+
 @end
 
 NS_ASSUME_NONNULL_END
