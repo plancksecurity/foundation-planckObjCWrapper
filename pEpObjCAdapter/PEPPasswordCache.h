79ced86b9bf6c64523ffc50605fb3802ff0ece74
Dirk Zimmermann <dz@pep.security>
2020-06-25 17:08:21 +0200

    IOSAD-172 Start password cache

---
+++ pEpObjCAdapter/PEPPasswordCache.h
@@ -0,0 +1,17 @@
+//
+//  PEPPasswordCache.h
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 25.06.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPPasswordCache : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ pEpObjCAdapter/PEPPasswordCache.m
@@ -0,0 +1,13 @@
+//
+//  PEPPasswordCache.m
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 25.06.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import "PEPPasswordCache.h"
+
+@implementation PEPPasswordCache
+
+@end
