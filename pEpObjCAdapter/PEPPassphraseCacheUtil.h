c20853e969a112e9b7b9bf8c0fe629542a5b280a
Dirk Zimmermann <dz@pep.security>
2020-06-26 16:54:34 +0200

    IOSAD-172 Util class for passphrase support

---
+++ pEpObjCAdapter/PEPPassphraseCacheUtil.h
@@ -0,0 +1,17 @@
+//
+//  PEPPassphraseCacheUtil.h
+//  PEPObjCAdapterFramework
+//
+//  Created by Dirk Zimmermann on 26.06.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPPassphraseCacheUtil : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ pEpObjCAdapter/PEPPassphraseCacheUtil.m
@@ -0,0 +1,13 @@
+//
+//  PEPPassphraseCacheUtil.m
+//  PEPObjCAdapterFramework
+//
+//  Created by Dirk Zimmermann on 26.06.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import "PEPPassphraseCacheUtil.h"
+
+@implementation PEPPassphraseCacheUtil
+
+@end
