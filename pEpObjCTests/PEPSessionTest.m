2396b86c41e00c8b452557cf4a6516368f1bee02
Dirk Zimmermann <dz@pep.security>
2020-07-02 17:14:52 +0200

    IOSAD-172 Test retry beacon after configurePassphrase

--- pEpObjCTests/PEPSessionTest.m
+++ pEpObjCTests/PEPSessionTest.m
@@ -1621,8 +1621,23 @@
 
     XCTAssertTrue(self.notifyHandshakeDelegate.notifyHandshakePassphraseRequired);
     self.notifyHandshakeDelegate.notifyHandshakePassphraseRequired = NO;
+
+    XCTAssertEqual(self.sendMessageDelegate.messages.count, 0);
+    XCTAssertNil(self.sendMessageDelegate.lastMessage);
+
+    XCTKVOExpectation *expHaveMessage1 = [[XCTKVOExpectation alloc]
+                                          initWithKeyPath:@"lastMessage"
+                                          object:self.sendMessageDelegate];
+
     XCTAssertTrue([session configurePassphrase:correctPassphrase error:&error]);
 
+    [self waitForExpectations:@[expHaveMessage1] timeout:PEPTestInternalSyncTimeout];
+
+    XCTAssertNotNil(self.sendMessageDelegate.lastMessage);
+    XCTAssertGreaterThan(self.sendMessageDelegate.messages.count, 0);
+
+    XCTAssertEqualObjects(self.sendMessageDelegate.lastMessage.from.address,
+                          identMe.address);
     [self shutdownSync];
 }
 
