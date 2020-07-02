6bfc93683fea19974d373b0f674fb184d5e3778a
Dirk Zimmermann <dz@pep.security>
2020-07-02 12:06:40 +0200

    IOSAD-172 WIP Passphrase cache reset

---
+++ pEpObjCTests/TestUtils/PEPPassphraseCache+Reset.m
@@ -0,0 +1,18 @@
+//
+//  PEPPassphraseCache+Reset.m
+//  pEpObjCAdapterTests
+//
+//  Created by Dirk Zimmermann on 02.07.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import "PEPPassphraseCache+Reset.h"
+
+@implementation PEPPassphraseCache (Reset)
+
++ (void)reset
+{
+    // TODO
+}
+
+@end
