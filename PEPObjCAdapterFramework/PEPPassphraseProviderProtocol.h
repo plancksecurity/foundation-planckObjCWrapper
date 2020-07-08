fe8436da391b6e5730287cb7cb68a4bcc5a13c89
Dirk Zimmermann <dz@pep.security>
2020-07-08 21:38:12 +0200

    IOSAD-177 New header file for PEPPassphraseProviderProtocol

---
+++ PEPObjCAdapterFramework/PEPPassphraseProviderProtocol.h
@@ -0,0 +1,17 @@
+//
+//  PEPPassphraseProviderProtocol.h
+//  PEPObjCAdapterFramework
+//
+//  Created by Dirk Zimmermann on 08.07.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol PEPPassphraseProviderProtocol <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
