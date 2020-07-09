f32a7e643812caa334a279f9b32f276720577863
Dirk Zimmermann <dz@pep.security>
2020-07-09 09:10:51 +0200

    IOSAD-177 Docs PEPPassphraseProviderProtocol

--- PEPObjCAdapterFramework/PEPPassphraseProviderProtocol.h
+++ PEPObjCAdapterFramework/PEPPassphraseProviderProtocol.h
@@ -10,17 +10,41 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-/// Callback an app can use to provide a passphrase to the adapter after user input.
+/// Delegate methods that an app can utilize to provide passphrases to the adapter after
+/// asking the user.
 ///
-/// A nil passphrase means to throw the original error
-/// (PEPStatusPassphraseRequired or PEPStatusWrongPassphrase), otherwise
-/// the current call is tried again with the given passphrase.
+/// If this delegate (passphrase provider) exists (see `+ [PEPObjCAdapter passphraseProvider]`),
+/// then any supported engine call that returns the engine equivalent of
+/// PEPStatusPassphraseRequired or PEPStatusWrongPassphrase
+/// leads to an invocation of the delegate, which then can indicate the user's respone
+/// via the given callback.
+///
+/// If no passphrase provider exists, the error status is thrown directly to
+/// the caller.
 typedef void (^PEPPassphraseProviderCallback)(NSString * _Nullable passphrase);
 
 @protocol PEPPassphraseProviderProtocol <NSObject>
 
+/// Called by the adapter when the engine signals PEPStatusPassphraseRequired.
+///
+/// See `PEPPassphraseProviderProtocol` for a general description.
+///
+/// @param completion Callback that either retries the engine call that lead
+/// to the adapter calling into the PEPPassphraseProviderProtocol delegate,
+/// or, if the given passphrase is nil, return the error to the caller.
 - (void)passphraseRequiredCompletion:(PEPPassphraseProviderCallback)completion;
+
+/// Called by the adapter when the engine signals PEPStatusWrongPassphrase.
+///
+/// See `PEPPassphraseProviderProtocol` for a general description.
+///
+/// @param completion Callback that either retries the engine call that lead
+/// to the adapter calling into the PEPPassphraseProviderProtocol delegate,
+/// or, if the given passphrase is nil, return the error to the caller.
 - (void)wrongPassphraseCompletion:(PEPPassphraseProviderCallback)completion;
+
+/// Signals that the passphrase indicated by the callback in one of the calls of
+/// this delegate was too long and cannot be used.
 - (void)passphraseTooLong:(PEPPassphraseProviderCallback)completion;
 
 @end
