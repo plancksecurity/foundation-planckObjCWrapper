e44af39dece6d51128f64f1f4d83db3adbbc8a03
Dirk Zimmermann <dz@pep.security>
2020-07-15 16:54:31 +0200

    IOS-2325 Back out: (Non-sensical) UI test for passphrases

--- pEpObjCTests/TestUtils/PEPPassphraseProviderUIMock.m
+++ pEpObjCTests/TestUtils/PEPPassphraseProviderUIMock.m
@@ -8,35 +8,19 @@
 
 #import "PEPPassphraseProviderUIMock.h"
 
-#import <UIKit/UIKit.h>
-
 @implementation PEPPassphraseProviderUIMock
 
 - (void)passphraseRequired:(nonnull PEPPassphraseProviderCallback)completion {
-    [self askUserForPassphrase:completion];
 }
 
 - (void)passphraseTooLong:(nonnull PEPPassphraseProviderCallback)completion {
-    [self askUserForPassphrase:completion];
 }
 
 - (void)wrongPassphrase:(nonnull PEPPassphraseProviderCallback)completion {
-    [self askUserForPassphrase:completion];
 }
 
 - (void)answerWithLatestPassphrase:(nonnull PEPPassphraseProviderCallback)completion
 {
-    [self askUserForPassphrase:completion];
-}
-
-- (void)askUserForPassphrase:(nonnull PEPPassphraseProviderCallback)completion
-{
-    UIAlertController *alert = [UIAlertController
-                                alertControllerWithTitle:@"Passphrase"
-                                message:@"Passphrase needed"
-                                preferredStyle:UIAlertControllerStyleActionSheet];
-    [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
-    }];
 }
 
 @end
