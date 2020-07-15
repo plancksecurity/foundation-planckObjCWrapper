a6b7053f5dedaf4513d983077cba058f1ea1e89b
Dirk Zimmermann <dz@pep.security>
2020-07-15 16:54:31 +0200

    IOS-2325 Add emtpy PEPPassphraseProviderUIMock

---
+++ pEpObjCTests/TestUtils/PEPPassphraseProviderUIMock.h
@@ -0,0 +1,19 @@
+//
+//  PEPPassphraseProviderUIMock.h
+//  pEpObjCAdapterTests
+//
+//  Created by Dirk Zimmermann on 15.07.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PEPPassphraseProviderProtocol.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPPassphraseProviderUIMock : NSObject<PEPPassphraseProviderProtocol>
+
+@end
+
+NS_ASSUME_NONNULL_END
