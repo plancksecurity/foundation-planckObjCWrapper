f35c2d5c2b08eba6462b6a250dd2d54dfe5c460a
Dirk Zimmermann <dz@pep.security>
2020-07-17 15:29:14 +0200

    IOSAD-178 More message util

---
+++ pEpObjCAdapter/PEPMessage+Internal.m
@@ -0,0 +1,13 @@
+//
+//  PEPMessage+Internal.m
+//  PEPObjCAdapterFramework
+//
+//  Created by Dirk Zimmermann on 17.07.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import "PEPMessage+Internal.h"
+
+@implementation PEPMessage (Internal)
+
+@end
