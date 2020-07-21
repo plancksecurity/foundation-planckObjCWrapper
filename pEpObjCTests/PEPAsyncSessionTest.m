8e5dc6928af692bbd19ca3c7a5d819176df31039
Dirk Zimmermann <dz@pep.security>
2020-07-21 16:57:44 +0200

    IOSAD-178 Wrap trustPersonalKey

--- pEpObjCTests/PEPAsyncSessionTest.m
+++ pEpObjCTests/PEPAsyncSessionTest.m
@@ -518,4 +518,26 @@
     return result;
 }
 
+- (BOOL)trustPersonalKey:(PEPIdentity * _Nonnull)identity
+                   error:(NSError * _Nullable * _Nullable)error
+{
+    PEPAsyncSession *asyncSession = [PEPAsyncSession new];
+    XCTestExpectation *exp = [self expectationWithDescription:@"exp"];
+    __block BOOL result = NO;
+    __block NSError *theError = nil;
+    [asyncSession trustPersonalKey:identity
+                     errorCallback:^(NSError * _Nonnull error) {
+        XCTFail();
+        result = NO;
+        theError = error;
+        [exp fulfill];
+    } successCallback:^{
+        result = YES;
+        [exp fulfill];
+    }];
+    [self waitForExpectations:@[exp] timeout:PEPTestInternalSyncTimeout];
+    *error = theError;
+    return result;
+}
+
 @end
