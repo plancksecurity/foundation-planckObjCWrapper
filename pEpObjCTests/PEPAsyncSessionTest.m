94ea68af99baf1483e13c127ba397a8b0d0075dc
Dirk Zimmermann <dz@pep.security>
2020-07-21 16:57:44 +0200

    IOSAD-178 Wrap keyResetTrust

--- pEpObjCTests/PEPAsyncSessionTest.m
+++ pEpObjCTests/PEPAsyncSessionTest.m
@@ -540,4 +540,26 @@
     return result;
 }
 
+- (BOOL)keyResetTrust:(PEPIdentity * _Nonnull)identity
+                error:(NSError * _Nullable * _Nullable)error
+{
+    PEPAsyncSession *asyncSession = [PEPAsyncSession new];
+    XCTestExpectation *exp = [self expectationWithDescription:@"exp"];
+    __block BOOL result = NO;
+    __block NSError *theError = nil;
+    [asyncSession keyResetTrust:identity
+                  errorCallback:^(NSError * _Nonnull error) {
+        XCTFail();
+        result = NO;
+        theError = error;
+        [exp fulfill];
+    } successCallback:^{
+        result = YES;
+        [exp fulfill];
+    }];
+    [self waitForExpectations:@[exp] timeout:PEPTestInternalSyncTimeout];
+    *error = theError;
+    return result;
+}
+
 @end
