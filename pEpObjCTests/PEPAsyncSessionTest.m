c68487e87c7878ca39f1e320709f8167c18a898b
Dirk Zimmermann <dz@pep.security>
2020-07-22 17:02:24 +0200

    IOSAD-178 Back out: Back out: Test wrap keyReset

--- pEpObjCTests/PEPAsyncSessionTest.m
+++ pEpObjCTests/PEPAsyncSessionTest.m
@@ -968,4 +968,29 @@
     return result;
 }
 
+- (BOOL)keyReset:(PEPIdentity * _Nonnull)identity
+     fingerprint:(NSString * _Nullable)fingerprint
+           error:(NSError * _Nullable * _Nullable)error
+{
+    PEPAsyncSession *asyncSession = [PEPAsyncSession new];
+    XCTestExpectation *exp = [self expectationWithDescription:@"exp"];
+    __block BOOL result = NO;
+    __block NSError *theError = nil;
+    [asyncSession keyReset:identity
+               fingerprint:fingerprint
+             errorCallback:^(NSError * _Nonnull error) {
+        result = NO;
+        theError = error;
+        [exp fulfill];
+    } successCallback:^{
+        result = YES;
+        [exp fulfill];
+    }];
+    [self waitForExpectations:@[exp] timeout:PEPTestInternalSyncTimeout];
+    if (error) {
+        *error = theError;
+    }
+    return result;
+}
+
 @end
