aaae08092207c3a2dbd47d9a4886a86eaf6a07f5
Dirk Zimmermann <dz@pep.security>
2020-10-21 16:56:19 +0200

    IOS-1673 NSArray extension

---
+++ pEpObjCAdapter/NSArray+PEP.h
@@ -0,0 +1,17 @@
+//
+//  NSArray+PEP.h
+//  pEpiOSAdapter
+//
+//  Created by Dirk Zimmermann on 21.10.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSArray (PEP)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ pEpObjCAdapter/NSArray+PEP.m
@@ -0,0 +1,13 @@
+//
+//  NSArray+PEP.m
+//  pEpiOSAdapter
+//
+//  Created by Dirk Zimmermann on 21.10.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@implementation NSArray (PEP)
+
+@end
