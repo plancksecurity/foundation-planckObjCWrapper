2d259f41bb6f314628f323296c0209f06525ccc4
Dirk Zimmermann <dz@pep.security>
2020-10-21 16:56:19 +0200

    IOS-1673 NSArray.arrayFromStringlist

--- pEpObjCAdapter/NSArray+PEP.h
+++ pEpObjCAdapter/NSArray+PEP.h
@@ -8,9 +8,13 @@
 
 #import <Foundation/Foundation.h>
 
+#import "message_api.h"
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSArray (PEP)
+
++ (NSArray * _Nonnull)arrayFromStringlist:(stringlist_t * _Nonnull)stringList;
 
 @end
 
--- pEpObjCAdapter/NSArray+PEP.m
+++ pEpObjCAdapter/NSArray+PEP.m
@@ -8,6 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
+#import "NSArray+PEP.h"
+
 @implementation NSArray (PEP)
+
++ (NSArray * _Nonnull)arrayFromStringlist:(stringlist_t * _Nonnull)stringList
+{
+    NSMutableArray *array = [NSMutableArray array];
+
+    for (stringlist_t *_sl = stringList; _sl && _sl->value; _sl = _sl->next) {
+        [array addObject:[NSString stringWithUTF8String:_sl->value]];
+    }
+
+    return array;
+}
 
 @end
