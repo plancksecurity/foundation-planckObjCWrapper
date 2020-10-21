1109121c84f29b6ca9586f584838edb23c109252
Dirk Zimmermann <dz@pep.security>
2020-10-21 16:56:19 +0200

    IOS-1673 PEPIdentity+Engine

---
+++ pEpObjCAdapter/Extensions/PEPIdentity+Engine.h
@@ -0,0 +1,19 @@
+//
+//  PEPIdentity+Engine.h
+//  pEpiOSAdapter
+//
+//  Created by Dirk Zimmermann on 21.10.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPIdentity.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPIdentity (Engine)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ pEpObjCAdapter/Extensions/PEPIdentity+Engine.m
@@ -0,0 +1,15 @@
+//
+//  PEPIdentity+Engine.m
+//  pEpiOSAdapter
+//
+//  Created by Dirk Zimmermann on 21.10.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPIdentity+Engine.h"
+
+@implementation PEPIdentity (Engine)
+
+@end
