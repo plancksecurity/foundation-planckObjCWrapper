1ddaa46a33fcc31cccc4820e469b66a53c22fdfd
Dirk Zimmermann <dz@pep.security>
2020-10-21 16:56:19 +0200

    IOS-1673 PEPMessage+Engine

---
+++ pEpObjCAdapter/Extensions/PEPMessage+Engine.h
@@ -0,0 +1,19 @@
+//
+//  PEPMessage+Engine.h
+//  pEpiOSAdapter
+//
+//  Created by Dirk Zimmermann on 21.10.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPMessage.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPMessage (Engine)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ pEpObjCAdapter/Extensions/PEPMessage+Engine.m
@@ -0,0 +1,15 @@
+//
+//  PEPMessage+Engine.m
+//  pEpiOSAdapter
+//
+//  Created by Dirk Zimmermann on 21.10.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPMessage+Engine.h"
+
+@implementation PEPMessage (Engine)
+
+@end
