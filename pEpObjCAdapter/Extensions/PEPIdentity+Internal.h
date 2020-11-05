c682ea67d7f85b4b9b01ff3f6f60cf4fc6100038
Dirk Zimmermann <dz@pep.security>
2020-11-05 10:09:44 +0100

    IOSAD-128 PEPIdentity+Internal

---
+++ pEpObjCAdapter/Extensions/PEPIdentity+Internal.h
@@ -0,0 +1,19 @@
+//
+//  PEPIdentity+Internal.h
+//  pEpiOSAdapter
+//
+//  Created by Dirk Zimmermann on 05.11.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPIdentity.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPIdentity (Internal)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ pEpObjCAdapter/Extensions/PEPIdentity+Internal.m
@@ -0,0 +1,15 @@
+//
+//  PEPIdentity+Internal.m
+//  pEpiOSAdapter
+//
+//  Created by Dirk Zimmermann on 05.11.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPIdentity+Internal.h"
+
+@implementation PEPIdentity (Internal)
+
+@end
