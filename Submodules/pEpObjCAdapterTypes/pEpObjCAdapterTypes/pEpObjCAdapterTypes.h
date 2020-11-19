9bfb70329526df1553699205831082f60fda13d1
Dirk Zimmermann <dz@pep.security>
2020-11-19 17:46:53 +0100

    IOSAD-185 New static lib: pEpObjCAdapterTypes

---
+++ Submodules/pEpObjCAdapterTypes/pEpObjCAdapterTypes/pEpObjCAdapterTypes.h
@@ -0,0 +1,12 @@
+//
+//  pEpObjCAdapterTypes.h
+//  pEpObjCAdapterTypes
+//
+//  Created by Dirk Zimmermann on 19.11.20.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface pEpObjCAdapterTypes : NSObject
+
+@end
