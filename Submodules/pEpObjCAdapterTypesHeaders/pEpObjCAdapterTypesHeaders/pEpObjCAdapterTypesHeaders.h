f043cb73c932973a9e7b07371c7fa8b610f27644
Dirk Zimmermann <dz@pep.security>
2020-11-19 17:46:53 +0100

    IOSAD-185 Add pEpObjCAdapterTypesHeaders

---
+++ Submodules/pEpObjCAdapterTypesHeaders/pEpObjCAdapterTypesHeaders/pEpObjCAdapterTypesHeaders.h
@@ -0,0 +1,18 @@
+//
+//  pEpObjCAdapterTypesHeaders.h
+//  pEpObjCAdapterTypesHeaders
+//
+//  Created by Dirk Zimmermann on 19.11.20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for pEpObjCAdapterTypesHeaders.
+FOUNDATION_EXPORT double pEpObjCAdapterTypesHeadersVersionNumber;
+
+//! Project version string for pEpObjCAdapterTypesHeaders.
+FOUNDATION_EXPORT const unsigned char pEpObjCAdapterTypesHeadersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pEpObjCAdapterTypesHeaders/PublicHeader.h>
+
+
