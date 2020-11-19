21a0f3570fa882ff7eedcdd9b0b85a1f9fbfd52a
Dirk Zimmermann <dz@pep.security>
2020-11-19 17:46:53 +0100

    IOSAD-185 Put PEPContentDisposition into own export

--- pEpObjCAdapterFramework/PEPObjCAdapterFramework.h
+++ pEpObjCAdapterFramework/PEPObjCAdapterFramework.h
@@ -35,6 +35,7 @@ FOUNDATION_EXPORT const unsigned char PEPObjCAdapterFrameworkVersionString[];
 #import <PEPObjCAdapterFramework/PEPRating.h>
 #import <PEPObjCAdapterFramework/PEPEncFormat.h>
 #import <PEPObjCAdapterFramework/PEPColor.h>
+#import <PEPObjCAdapterFramework/PEPContentDisposition.h>
 
 #import <pEpObjCAdapterTypesHeaders/PEPAttachment.h>
 #import <pEpObjCAdapterTypesHeaders/PEPColor.h>
