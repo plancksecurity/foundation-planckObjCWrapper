186de291a712ed5526ae8c7807de664853035aeb
Dirk Zimmermann <dz@pep.security>
2020-11-19 17:46:53 +0100

    IOSAD-185 Re-add the wizard generated files, to have sample content

---
+++ Submodules/pEpObjCAdapterTypes/pEpObjCAdapterTypes/pEpObjCAdapterTypes.m
@@ -0,0 +1,12 @@
+//
+//  pEpObjCAdapterTypes.m
+//  pEpObjCAdapterTypes
+//
+//  Created by Dirk Zimmermann on 19.11.20.
+//
+
+#import "pEpObjCAdapterTypes.h"
+
+@implementation pEpObjCAdapterTypes
+
+@end
