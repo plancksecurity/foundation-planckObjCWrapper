d1f9df946180dddf8a717c26728549c139907486
Dirk Zimmermann <dz@pep.security>
2020-11-19 17:46:53 +0100

    IOSAD-185 Note headers in umbrella header

--- Submodules/pEpObjCAdapterTypesHeaders/pEpObjCAdapterTypesHeaders/pEpObjCAdapterTypesHeaders.h
+++ Submodules/pEpObjCAdapterTypesHeaders/pEpObjCAdapterTypesHeaders/pEpObjCAdapterTypesHeaders.h
@@ -15,4 +15,13 @@ FOUNDATION_EXPORT const unsigned char pEpObjCAdapterTypesHeadersVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <pEpObjCAdapterTypesHeaders/PublicHeader.h>
 
-
+#import <pEpObjCAdapterTypesHeaders/PEPAttachment.h>
+#import <pEpObjCAdapterTypesHeaders/PEPColor.h>
+#import <pEpObjCAdapterTypesHeaders/PEPDecryptFlags.h>
+#import <pEpObjCAdapterTypesHeaders/PEPEncFormat.h>
+#import <pEpObjCAdapterTypesHeaders/PEPIdentity.h>
+#import <pEpObjCAdapterTypesHeaders/PEPLanguage.h>
+#import <pEpObjCAdapterTypesHeaders/PEPMessage.h>
+#import <pEpObjCAdapterTypesHeaders/PEPRating.h>
+#import <pEpObjCAdapterTypesHeaders/PEPStringList.h>
+#import <pEpObjCAdapterTypesHeaders/PEPSyncHandshakeResult.h>
