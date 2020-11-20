f2cd3f546b3929bae14532af8e9670757cdec2be
Dirk Zimmermann <dz@pep.security>
2020-11-20 16:00:04 +0100

    IOSAD-185 Add pEpObjCAdapterTypes static lib

---
+++ Submodules/pEpObjCAdapterTypes/pEpObjCAdapterTypes/pEpObjCAdapterTypes.m
@@ -0,0 +1,12 @@
+//
+//  pEpObjCAdapterTypes.m
+//  pEpObjCAdapterTypes
+//
+//  Created by Dirk Zimmermann on 20.11.20.
+//
+
+#import "pEpObjCAdapterTypes.h"
+
+@implementation pEpObjCAdapterTypes
+
+@end
