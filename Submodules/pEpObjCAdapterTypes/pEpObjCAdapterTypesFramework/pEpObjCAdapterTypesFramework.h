be81b85190d37c1c1f91c21bd3595b49f8df2b9e
Dirk Zimmermann <dz@pep.security>
2020-11-30 17:01:03 +0100

    IOSAD-185 Add types framework

---
+++ Submodules/pEpObjCAdapterTypes/pEpObjCAdapterTypesFramework/pEpObjCAdapterTypesFramework.h
@@ -0,0 +1,18 @@
+//
+//  pEpObjCAdapterTypesFramework.h
+//  pEpObjCAdapterTypesFramework
+//
+//  Created by Dirk Zimmermann on 30.11.20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for pEpObjCAdapterTypesFramework.
+FOUNDATION_EXPORT double pEpObjCAdapterTypesFrameworkVersionNumber;
+
+//! Project version string for pEpObjCAdapterTypesFramework.
+FOUNDATION_EXPORT const unsigned char pEpObjCAdapterTypesFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pEpObjCAdapterTypesFramework/PublicHeader.h>
+
+
