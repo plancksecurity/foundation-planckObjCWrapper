6497a5dbf6d973784563c6b55f8749db47f05514
Dirk Zimmermann <dz@pep.security>
2020-11-30 17:01:03 +0100

    IOSAD-185 Import PEPObjCAdapterTypesFramework

--- pEpObjCTests/PepTests.m
+++ pEpObjCTests/PepTests.m
@@ -8,7 +8,8 @@
 
 #import <XCTest/XCTest.h>
 
-#import "PEPLanguage.h"
+#import <PEPObjCAdapterTypesFramework/PEPObjCAdapterTypesFramework.h>
+
 #import "PEPObjCAdapter.h"
 #import "PEPInternalSession.h"
 #import "PEPSessionProvider.h"
