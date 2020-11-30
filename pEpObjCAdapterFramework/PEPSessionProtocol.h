c39dd66c0c8823f59fe1d08eafb54cd694d47c7e
Dirk Zimmermann <dz@pep.security>
2020-11-30 17:01:03 +0100

    IOSAD-185 No PEPColor

--- pEpObjCAdapterFramework/PEPSessionProtocol.h
+++ pEpObjCAdapterFramework/PEPSessionProtocol.h
@@ -14,7 +14,6 @@
 
 #import <PEPObjCAdapterFramework/PEPTypes.h>
 #import <PEPObjCAdapterFramework/PEPEngineTypes.h>
-#import <PEPObjCAdapterFramework/PEPColor.h>
 
 @class PEPLanguage;
 @class PEPIdentity;
