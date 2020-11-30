6ed84830bd93da0ab464459c69b865fe894a863e
Dirk Zimmermann <dz@pep.security>
2020-11-30 17:01:03 +0100

    IOSAD-185 Full framework import

--- pEpObjCTests/PEPInternalSessionTest.m
+++ pEpObjCTests/PEPInternalSessionTest.m
@@ -8,7 +8,7 @@
 
 #import <XCTest/XCTest.h>
 
-#import "PEPObjCAdapterFramework.h"
+#import <PEPObjCAdapterFramework/PEPObjCAdapterFramework.h>
 
 #import "PEPObjCAdapter.h"
 #import "PEPTestUtils.h"
