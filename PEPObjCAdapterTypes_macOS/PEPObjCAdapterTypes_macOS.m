c7999e8862e39cc99b5c3ecde30991510f6e02ea
buff <andreas@pep-project.org>
2020-11-30 17:59:31 +0100

    adds macOS lib target

---
+++ PEPObjCAdapterTypes_macOS/PEPObjCAdapterTypes_macOS.m
@@ -0,0 +1,13 @@
+//
+//  PEPObjCAdapterTypes_macOS.m
+//  PEPObjCAdapterTypes_macOS
+//
+//  Created by Andreas Buff on 30.11.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import "PEPObjCAdapterTypes_macOS.h"
+
+@implementation PEPObjCAdapterTypes_macOS
+
+@end
