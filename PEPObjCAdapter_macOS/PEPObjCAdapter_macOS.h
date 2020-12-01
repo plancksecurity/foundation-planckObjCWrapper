c8c36b8c638ecd2229fdb1fe1b5ad24e2b1fca60
buff <andreas@pep-project.org>
2020-12-01 12:28:37 +0100

    adds ObjCAdapter static lib target

---
+++ PEPObjCAdapter_macOS/PEPObjCAdapter_macOS.h
@@ -0,0 +1,13 @@
+//
+//  PEPObjCAdapter_macOS.h
+//  PEPObjCAdapter_macOS
+//
+//  Created by Andreas Buff on 01.12.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PEPObjCAdapter_macOS : NSObject
+
+@end
