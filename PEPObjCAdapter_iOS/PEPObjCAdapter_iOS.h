4fc3b97156f001104629a48d890f075bae9da9d5
buff <andreas@pep-project.org>
2020-12-03 17:17:47 +0100

    IOSAD-186 adds ObjCAdapter static lib target

---
+++ PEPObjCAdapter_iOS/PEPObjCAdapter_iOS.h
@@ -0,0 +1,13 @@
+//
+//  PEPObjCAdapter_iOS.h
+//  PEPObjCAdapter_iOS
+//
+//  Created by Andreas Buff on 03.12.20.
+//  Copyright © 2020 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PEPObjCAdapter_iOS : NSObject
+
+@end
