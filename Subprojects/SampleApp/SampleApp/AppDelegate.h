1a9ffc1753a0f1bd590cc3ea74371b8001db3166
Andreas Buff <andreas@pep-security.net>
2021-01-22 15:49:07 +0100

    IOSAD-186 cleanup

---
+++ Subprojects/SampleApp/SampleApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SampleApp
+//
+//  Created by Andreas Buff on 22.01.21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Subprojects/SampleApp/SampleApp/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  SampleApp
+//
+//  Created by Andreas Buff on 22.01.21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
