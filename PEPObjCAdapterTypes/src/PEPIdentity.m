bc7d0a4967d03bd37bd4398abaef359725ffc3fd
David Alarcon <momachilles@gmail.com>
2021-01-25 16:48:43 +0100

    IOSAD-191 NSSecureCoding in PEPIdentity, not category.

--- PEPObjCAdapterTypes/src/PEPIdentity.m
+++ PEPObjCAdapterTypes/src/PEPIdentity.m
@@ -124,4 +124,36 @@ static NSArray *s_keys;
     s_keys = @[@"address"];
 }
 
+// MARK: - NSSecureCoding
+
+- (void)encodeWithCoder:(nonnull NSCoder *)coder {
+    [coder encodeObject:self.address        forKey:@"address"];
+    [coder encodeObject:self.userID         forKey:@"userID"];
+    [coder encodeObject:self.userName       forKey:@"userName"];
+    [coder encodeObject:self.fingerPrint    forKey:@"fingerPrint"];
+    [coder encodeObject:self.language       forKey:@"language"];
+    [coder encodeInt:self.commType          forKey:@"commType"];
+    [coder encodeBool:self.isOwn            forKey:@"isOwn"];
+    [coder encodeInt:self.flags             forKey:@"flags"];
+}
+
+- (nullable instancetype)initWithCoder:(nonnull NSCoder *)decoder {
+    if (self = [self init]) {
+        self.address = [decoder decodeObjectOfClass:[NSString class] forKey:@"address"];
+        self.userID = [decoder decodeObjectOfClass:[NSString class] forKey:@"userID"];
+        self.userName = [decoder decodeObjectOfClass:[NSString class] forKey:@"userName"];
+        self.fingerPrint = [decoder decodeObjectOfClass:[NSString class] forKey:@"fingerPrint"];
+        self.language = [decoder decodeObjectOfClass:[NSString class] forKey:@"language"];
+        self.commType = [decoder decodeIntForKey:@"commType"];
+        self.isOwn = [decoder decodeBoolForKey:@"isOwn"];
+        self.flags = [decoder decodeIntForKey:@"flags"];
+    }
+
+    return self;
+}
+
++ (BOOL)supportsSecureCoding {
+    return  YES;
+}
+
 @end
