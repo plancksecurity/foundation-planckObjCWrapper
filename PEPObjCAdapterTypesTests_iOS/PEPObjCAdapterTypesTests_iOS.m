1689cd4d53ecb37f495d58ea9ea079f0d3386375
David Alarcon <momachilles@gmail.com>
2021-01-27 14:45:16 +0100

    IOSAD-191 Add PEPObjCAdapterTypesTests_iOS target.

---
+++ PEPObjCAdapterTypesTests_iOS/PEPObjCAdapterTypesTests_iOS.m
@@ -0,0 +1,37 @@
+//
+//  PEPObjCAdapterTypesTests_iOS.m
+//  PEPObjCAdapterTypesTests_iOS
+//
+//  Created by David Alarcon on 27/1/21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PEPObjCAdapterTypesTests_iOS : XCTestCase
+
+@end
+
+@implementation PEPObjCAdapterTypesTests_iOS
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
