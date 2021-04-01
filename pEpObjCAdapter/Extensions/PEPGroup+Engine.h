1e9fd548d3b08fecd43b84908a9fd60ddea9d517
Dirk Zimmermann <dz@pep.security>
2021-04-01 15:53:12 +0200

    IOSAD-209 PEPGroup+Engine

---
+++ pEpObjCAdapter/Extensions/PEPGroup+Engine.h
@@ -0,0 +1,23 @@
+//
+//  PEPGroup+Engine.h
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 01.04.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "group.h"
+
+#import "PEPGroup.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class PEPMember;
+
+@interface PEPGroup (Engine)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ pEpObjCAdapter/Extensions/PEPGroup+Engine.m
@@ -0,0 +1,13 @@
+//
+//  PEPGroup+Engine.m
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 01.04.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import "PEPGroup+Engine.h"
+
+@implementation PEPGroup (Engine)
+
+@end
