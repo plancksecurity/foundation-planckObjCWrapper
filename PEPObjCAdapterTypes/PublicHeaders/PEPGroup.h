79b8bee1e75b0cdcba36124ce40d31c339ed3fc2
Dirk Zimmermann <dz@pep.security>
2021-04-01 15:22:44 +0200

    IOSAD-201 Add PEPGroup

---
+++ PEPObjCAdapterTypes/PublicHeaders/PEPGroup.h
@@ -0,0 +1,17 @@
+//
+//  PEPGroup.h
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 01.04.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPGroup : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PEPObjCAdapterTypes/src/PEPGroup.m
@@ -0,0 +1,13 @@
+//
+//  PEPGroup.m
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 01.04.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import "PEPGroup.h"
+
+@implementation PEPGroup
+
+@end
