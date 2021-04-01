3e49250d86497be94e1c8be281049b6a1b230a29
Dirk Zimmermann <dz@pep.security>
2021-04-01 11:34:09 +0200

    IOSAD-201 Create PEPMember

---
+++ PEPObjCAdapterTypes/PublicHeaders/PEPMember.m
@@ -0,0 +1,13 @@
+//
+//  PEPMember.m
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 01.04.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import "PEPMember.h"
+
+@implementation PEPMember
+
+@end
