aae52c55ddf414ac1a735439cff17bdb30e8e62e
Dirk Zimmermann <dz@pep.security>
2021-04-01 12:19:52 +0200

    IOSAD-201 Start PEPMember <-> engine

---
+++ pEpObjCAdapter/Extensions/PEPMember+Engine.m
@@ -0,0 +1,13 @@
+//
+//  PEPMember+Engine.m
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 01.04.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import "PEPMember+Engine.h"
+
+@implementation PEPMember (Engine)
+
+@end
