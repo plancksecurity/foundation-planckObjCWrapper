9a2b254e4ba9c41fa1f2e9d00c3e7cf94539d33f
Dirk Zimmermann <dz@pep.security>
2021-04-08 15:10:33 +0200

    IOSAD-201 Split group tests off

---
+++ pEpObjCTests/PEPInternalSessionGroupTest.m
@@ -0,0 +1,153 @@
+//
+//  PEPInternalSessionGroupTest.m
+//  pEpObjCAdapterTests
+//
+//  Created by Dirk Zimmermann on 08.04.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "PEPObjCAdapterTypes_iOS.h"
+#import "PEPObjCAdapter_iOS.h"
+
+#import "PEPSessionProvider.h"
+#import "PEPInternalSession.h"
+
+@interface PEPInternalSessionGroupTest : XCTestCase
+
+@end
+
+@implementation PEPInternalSessionGroupTest
+
+#pragma mark - Group API
+
+- (void)testGroupCreate
+{
+    PEPInternalSession *session = [PEPSessionProvider session];
+
+    PEPIdentity *identyGroup = [[PEPIdentity alloc]
+                                initWithAddress:@"group@pep.security"
+                                userID:@"group"
+                                userName:@"group"
+                                isOwn:YES];
+
+    PEPIdentity *identyManager = [[PEPIdentity alloc]
+                                  initWithAddress:@"manager@pep.security"
+                                  userID:@"manager"
+                                  userName:@"manager"
+                                  isOwn:NO];
+
+    PEPIdentity *identityMember1 = [[PEPIdentity alloc]
+                                    initWithAddress:@"member1@pep.security"
+                                    userID:@"member1"
+                                    userName:@"member1"
+                                    isOwn:NO];
+
+    NSError *error = nil;
+
+    for (PEPIdentity *ident in @[identyGroup]) {
+        error = nil;
+        XCTAssertTrue([session mySelf:ident error:&error]);
+        XCTAssertNil(error);
+    }
+
+    for (PEPIdentity *ident in @[identyManager, identityMember1]) {
+        error = nil;
+        XCTAssertTrue([session updateIdentity:ident error:&error]);
+        XCTAssertNil(error);
+    }
+
+    PEPGroup *group = [self createGroupWithIdentity:identyGroup
+                                      identyManager:identyManager
+                                            members:@[identityMember1]];
+
+    XCTAssertNotNil(group);
+}
+
+- (void)testGroupJoinNoMember
+{
+    PEPInternalSession *session = [PEPSessionProvider session];
+
+    PEPIdentity *identyGroup = [[PEPIdentity alloc]
+                                initWithAddress:@"group@pep.security"
+                                userID:@"group"
+                                userName:@"group"
+                                isOwn:YES];
+
+    PEPIdentity *identyManager = [[PEPIdentity alloc]
+                                  initWithAddress:@"manager@pep.security"
+                                  userID:@"manager"
+                                  userName:@"manager"
+                                  isOwn:NO];
+
+    PEPIdentity *identityMember1 = [[PEPIdentity alloc]
+                                    initWithAddress:@"member1@pep.security"
+                                    userID:@"member1"
+                                    userName:@"member1"
+                                    isOwn:NO];
+
+    NSError *error = nil;
+
+    for (PEPIdentity *ident in @[identyGroup]) {
+        error = nil;
+        XCTAssertTrue([session mySelf:ident error:&error]);
+        XCTAssertNil(error);
+    }
+
+    for (PEPIdentity *ident in @[identyManager, identityMember1]) {
+        error = nil;
+        XCTAssertTrue([session updateIdentity:ident error:&error]);
+        XCTAssertNil(error);
+    }
+
+    PEPGroup *group = [self createGroupWithIdentity:identyGroup
+                                      identyManager:identyManager
+                                            members:@[identityMember1]];
+
+    XCTAssertNotNil(group);
+
+    PEPIdentity *identityMember2 = [[PEPIdentity alloc]
+                                    initWithAddress:@"member2@pep.security"
+                                    userID:@"member2"
+                                    userName:@"member2"
+                                    isOwn:YES];
+
+
+    error = nil;
+    XCTAssertTrue([session mySelf:identityMember2 error:&error]);
+    XCTAssertNil(error);
+
+    XCTAssertFalse([session groupJoin:identyGroup asMemberIdentity:identityMember2 error:&error]);
+    XCTAssertNotNil(error);
+    XCTAssertEqual(error.code, PEP_NO_MEMBERSHIP_STATUS_FOUND);
+}
+
+#pragma mark - Helpers
+
+- (PEPGroup *)createGroupWithIdentity:(PEPIdentity *)identyGroup
+                        identyManager:(PEPIdentity *)identyManager
+                              members:(NSArray<PEPIdentity *> *)members
+{
+    PEPInternalSession *session = [PEPSessionProvider session];
+
+    PEPIdentity *identityMember1 = [[PEPIdentity alloc]
+                                    initWithAddress:@"member1@pep.security"
+                                    userID:@"member1"
+                                    userName:@"member1"
+                                    isOwn:NO];
+
+    NSError *error = nil;
+
+    PEPGroup *group = [session groupCreate:identyGroup
+                                   manager:identyManager
+                                   members:@[identityMember1]
+                                     error:&error];
+
+    XCTAssertNotNil(group);
+    XCTAssertNil(error);
+
+    return group;
+}
+
+@end
