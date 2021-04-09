86a56ed0c1fa93ded71dc53ddb6ab02237186ceb
Dirk Zimmermann <dz@pep.security>
2021-04-09 09:44:27 +0200

    IOSAD-201 Start XCTestCase (PEPSession)

---
+++ pEpObjCTests/TestUtils/XCTestCase+PEPSession.h
@@ -0,0 +1,19 @@
+//
+//  XCTestCase+PEPSession.h
+//  pEpObjCAdapterTests
+//
+//  Created by Dirk Zimmermann on 09.04.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <XCTest/XCTest.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCTestCase (PEPSession)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ pEpObjCTests/TestUtils/XCTestCase+PEPSession.m
@@ -0,0 +1,13 @@
+//
+//  XCTestCase+PEPSession.m
+//  pEpObjCAdapterTests
+//
+//  Created by Dirk Zimmermann on 09.04.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import "XCTestCase+PEPSession.h"
+
+@implementation XCTestCase (PEPSession)
+
+@end
