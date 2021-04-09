9d038b2a039e2deca86f1e1f5ae607ceb4c72eea
Dirk Zimmermann <dz@pep.security>
2021-04-09 10:19:44 +0200

    IOSAD-201 Separate the XCTest session helpers

--- pEpObjCTests/TestUtils/XCTestCase+PEPSession.h
+++ pEpObjCTests/TestUtils/XCTestCase+PEPSession.h
@@ -9,9 +9,72 @@
 #import <Foundation/Foundation.h>
 #import <XCTest/XCTest.h>
 
+#import "PEPObjCAdapterTypes_iOS.h"
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface XCTestCase (PEPSession)
+
+#pragma mark - Normal session to async
+
+- (PEPRating)ratingForIdentity:(PEPIdentity *)identity;
+
+- (PEPIdentity * _Nullable)mySelf:(PEPIdentity * _Nonnull)identity
+                            error:(NSError * _Nullable * _Nullable)error;
+
+- (NSArray<NSString *> * _Nullable)trustwordsForFingerprint:(NSString * _Nonnull)fingerprint
+                                                 languageID:(NSString * _Nonnull)languageID
+                                                  shortened:(BOOL)shortened
+                                                      error:(NSError * _Nullable * _Nullable)error;
+
+- (PEPIdentity * _Nullable)updateIdentity:(PEPIdentity * _Nonnull)identity
+                                    error:(NSError * _Nullable * _Nullable)error;
+
+- (NSNumber * _Nullable)outgoingRatingForMessage:(PEPMessage * _Nonnull)theMessage
+                                           error:(NSError * _Nullable * _Nullable)error;
+
+- (PEPMessage * _Nullable)encryptMessage:(PEPMessage * _Nonnull)message
+                               extraKeys:(PEPStringList * _Nullable)extraKeys
+                               encFormat:(PEPEncFormat)encFormat
+                                  status:(PEPStatus * _Nullable)status
+                                   error:(NSError * _Nullable * _Nullable)error;
+
+- (PEPMessage * _Nullable)encryptMessage:(PEPMessage * _Nonnull)message
+                               extraKeys:(PEPStringList * _Nullable)extraKeys
+                                  status:(PEPStatus * _Nullable)status
+                                   error:(NSError * _Nullable * _Nullable)error;
+
+- (BOOL)trustPersonalKey:(PEPIdentity * _Nonnull)identity
+                   error:(NSError * _Nullable * _Nullable)error;
+
+- (BOOL)keyResetTrust:(PEPIdentity * _Nonnull)identity
+                error:(NSError * _Nullable * _Nullable)error;
+
+- (BOOL)keyMistrusted:(PEPIdentity *)identity error:(NSError * _Nullable * _Nullable)error;
+
+- (BOOL)enableSyncForIdentity:(PEPIdentity * _Nonnull)identity
+                        error:(NSError * _Nullable * _Nullable)error;
+
+- (BOOL)disableSyncForIdentity:(PEPIdentity * _Nonnull)identity
+                         error:(NSError * _Nullable * _Nullable)error;
+
+- (NSString * _Nullable)getLogWithError:(NSError * _Nullable * _Nullable)error;
+
+- (NSString * _Nullable)getTrustwordsIdentity1:(PEPIdentity * _Nonnull)identity1
+                                     identity2:(PEPIdentity * _Nonnull)identity2
+                                      language:(NSString * _Nullable)language
+                                          full:(BOOL)full
+                                         error:(NSError * _Nullable * _Nullable)error;
+
+- (NSNumber * _Nullable)isPEPUser:(PEPIdentity * _Nonnull)identity
+                            error:(NSError * _Nullable * _Nullable)error;
+
+- (BOOL)trustOwnKeyIdentity:(PEPIdentity * _Nonnull)identity
+                      error:(NSError * _Nullable * _Nullable)error;
+
+- (BOOL)keyReset:(PEPIdentity * _Nonnull)identity
+     fingerprint:(NSString * _Nullable)fingerprint
+           error:(NSError * _Nullable * _Nullable)error;
 
 @end
 
