a43dd8a9c52d4c17ac20ca304f760774673eb526
Dirk Zimmermann <dz@pep.security>
2021-04-09 09:34:15 +0200

    IOSAD-201 Start PEPSessionGroupTest

---
+++ pEpObjCTests/PEPSessionGroupTest.m
@@ -0,0 +1,36 @@
+//
+//  PEPSessionGroupTest.m
+//  pEpObjCAdapterTests
+//
+//  Created by Dirk Zimmermann on 09.04.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "PEPTestUtils.h"
+
+@interface PEPSessionGroupTest : XCTestCase
+
+@end
+
+@implementation PEPSessionGroupTest
+
+- (void)setUp
+{
+    [super setUp];
+    [self pEpCleanUp];
+}
+
+- (void)tearDown
+{
+    [self pEpCleanUp];
+    [super tearDown];
+}
+
+- (void)pEpCleanUp
+{
+    [PEPTestUtils cleanUp];
+}
+
+@end
