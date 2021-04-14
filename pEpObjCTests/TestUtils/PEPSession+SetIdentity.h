3486a7986fc2bbcdae0ab8d64d81a1270e1d57c3
Dirk Zimmermann <dz@pep.security>
2021-04-14 11:36:01 +0200

    IOSAD-201 Start PEPSession+SetIdentity

---
+++ pEpObjCTests/TestUtils/PEPSession+SetIdentity.h
@@ -0,0 +1,19 @@
+//
+//  PEPSession+SetIdentity.h
+//  pEpObjCAdapterTests
+//
+//  Created by Dirk Zimmermann on 14.04.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPSession.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPSession (SetIdentity)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ pEpObjCTests/TestUtils/PEPSession+SetIdentity.m
@@ -0,0 +1,13 @@
+//
+//  PEPSession+SetIdentity.m
+//  pEpObjCAdapterTests
+//
+//  Created by Dirk Zimmermann on 14.04.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import "PEPSession+SetIdentity.h"
+
+@implementation PEPSession (SetIdentity)
+
+@end
