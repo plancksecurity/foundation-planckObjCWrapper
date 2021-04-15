e8b3d794fb841d759d4868cd31f3bbcc23a3d073
Dirk Zimmermann <dz@pep.security>
2021-04-15 08:18:09 +0200

    IOSAD-201 PEPSession testGroupDissolve

--- pEpObjCTests/PEPSessionGroupTest.m
+++ pEpObjCTests/PEPSessionGroupTest.m
@@ -151,4 +151,58 @@
     XCTAssertEqual(error.code, PEP_NO_MEMBERSHIP_STATUS_FOUND);
 }
 
+- (void)testGroupDissolve
+{
+    PEPIdentity *identyGroup = [[PEPIdentity alloc]
+                                initWithAddress:@"group@pep.security"
+                                userID:@"group"
+                                userName:@"group"
+                                isOwn:YES];
+
+    PEPIdentity *identyManager = [[PEPIdentity alloc]
+                                  initWithAddress:@"manager@pep.security"
+                                  userID:@"manager"
+                                  userName:@"manager"
+                                  isOwn:NO];
+
+    PEPIdentity *identityMember1 = [[PEPIdentity alloc]
+                                    initWithAddress:@"member1@pep.security"
+                                    userID:@"member1"
+                                    userName:@"member1"
+                                    isOwn:NO];
+
+    NSError *error = nil;
+
+    for (PEPIdentity *ident in @[identyGroup]) {
+        error = nil;
+        XCTAssertTrue([self mySelf:ident error:&error]);
+        XCTAssertNil(error);
+    }
+
+    for (PEPIdentity *ident in @[identyManager, identityMember1]) {
+        error = nil;
+        XCTAssertTrue([self updateIdentity:ident error:&error]);
+        XCTAssertNil(error);
+    }
+
+    error = nil;
+
+    PEPGroup *group = [self groupCreateGroupIdentity:identyGroup
+                                     managerIdentity:identyManager
+                                    memberIdentities:@[identityMember1]
+                                               error:&error];
+
+    XCTAssertNotNil(group);
+    XCTAssertNil(error);
+
+    XCTAssertNotNil(group);
+
+    error = nil;
+
+    XCTAssertTrue([self groupDissolveGroupIdentity:identyGroup
+                                   managerIdentity:identyManager
+                                             error:&error]);
+    XCTAssertNil(error);
+}
+
 @end
