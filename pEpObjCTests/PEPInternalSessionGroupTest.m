f2f3df746905cfc19e338e67160eaf799b2c3f74
Dirk Zimmermann <dz@pep.security>
2021-04-15 11:25:05 +0200

    IOSAD-201 Rename

--- pEpObjCTests/PEPInternalSessionGroupTest.m
+++ pEpObjCTests/PEPInternalSessionGroupTest.m
@@ -336,11 +336,11 @@
 {
     PEPInternalSession *session = [PEPSessionProvider session];
 
-    PEPIdentity *identyGroup = [[PEPIdentity alloc]
-                                initWithAddress:@"group@pep.security"
-                                userID:@"group"
-                                userName:@"group"
-                                isOwn:YES];
+    PEPIdentity *identityGroup = [[PEPIdentity alloc]
+                                  initWithAddress:@"group@pep.security"
+                                  userID:@"group"
+                                  userName:@"group"
+                                  isOwn:YES];
 
     PEPIdentity *identyManager = [[PEPIdentity alloc]
                                   initWithAddress:@"manager@pep.security"
@@ -356,7 +356,7 @@
 
     NSError *error = nil;
 
-    for (PEPIdentity *ident in @[identyGroup]) {
+    for (PEPIdentity *ident in @[identityGroup]) {
         error = nil;
         XCTAssertTrue([session mySelf:ident error:&error]);
         XCTAssertNil(error);
@@ -370,7 +370,7 @@
 
     error = nil;
 
-    PEPGroup *group = [session groupCreateGroupIdentity:identyGroup
+    PEPGroup *group = [session groupCreateGroupIdentity:identityGroup
                                         managerIdentity:identyManager
                                        memberIdentities:@[identityMember1]
                                                   error:&error];
@@ -381,7 +381,7 @@
     XCTAssertNotNil(group);
 
     error = nil;
-    NSNumber *ratingNumber = [session groupRatingGroupIdentity:identyGroup
+    NSNumber *ratingNumber = [session groupRatingGroupIdentity:identityGroup
                                                managerIdentity:identyManager
                                                          error:&error];
     XCTAssertNotNil(ratingNumber);
