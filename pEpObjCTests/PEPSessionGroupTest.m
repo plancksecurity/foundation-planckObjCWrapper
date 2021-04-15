52186f4ce32dcfed0ce32cc794821a164ed54f9d
Dirk Zimmermann <dz@pep.security>
2021-04-15 08:49:19 +0200

    IOSAD-201 PEPSession testGroupRemoveMember

--- pEpObjCTests/PEPSessionGroupTest.m
+++ pEpObjCTests/PEPSessionGroupTest.m
@@ -270,4 +270,57 @@
     XCTAssertEqual(error.code, PEP_NO_TRUST);
 }
 
+- (void)testGroupRemoveMember
+{
+    PEPIdentity *identyGroup = [[PEPIdentity alloc]
+                                initWithAddress:@"group@pep.security"
+                                userID:@"group"
+                                userName:@"group"
+                                isOwn:YES];
+
+    PEPIdentity *identyManager = [[PEPIdentity alloc]
+                                  initWithAddress:@"manager@pep.security"
+                                  userID:@"manager"
+                                  userName:@"manager"
+                                  isOwn:NO];
+
+    PEPIdentity *identityMember1 = [[PEPIdentity alloc]
+                                    initWithAddress:@"member1@pep.security"
+                                    userID:@"member1"
+                                    userName:@"member1"
+                                    isOwn:NO];
+
+    NSError *error = nil;
+
+    for (PEPIdentity *ident in @[identyGroup]) {
+        error = nil;
+        XCTAssertTrue([self mySelf:ident error:&error]);
+        XCTAssertNil(error);
+    }
+
+    for (PEPIdentity *ident in @[identyManager, identityMember1]) {
+        error = nil;
+        XCTAssertTrue([self updateIdentity:ident error:&error]);
+        XCTAssertNil(error);
+    }
+
+    error = nil;
+
+    PEPGroup *group = [self groupCreateGroupIdentity:identyGroup
+                                     managerIdentity:identyManager
+                                    memberIdentities:@[identityMember1]
+                                               error:&error];
+
+    XCTAssertNotNil(group);
+    XCTAssertNil(error);
+
+    XCTAssertNotNil(group);
+
+    error = nil;
+    XCTAssertTrue([self groupRemoveMemberGroupIdentity:identyGroup
+                                        memberIdentity:identityMember1
+                                                 error:&error]);
+    XCTAssertNil(error);
+}
+
 @end
