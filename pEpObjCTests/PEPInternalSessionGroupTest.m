71ec2471361fc21e5da3bc6d9f5d06713a3a1fb8
Dirk Zimmermann <dz@pep.security>
2021-04-15 10:12:23 +0200

    IOSAD-201 Rename

--- pEpObjCTests/PEPInternalSessionGroupTest.m
+++ pEpObjCTests/PEPInternalSessionGroupTest.m
@@ -50,11 +50,11 @@
                                   userName:@"group"
                                   isOwn:YES];
 
-    PEPIdentity *identyManager = [[PEPIdentity alloc]
-                                  initWithAddress:@"manager@pep.security"
-                                  userID:@"manager"
-                                  userName:@"manager"
-                                  isOwn:NO];
+    PEPIdentity *identityManager = [[PEPIdentity alloc]
+                                    initWithAddress:@"manager@pep.security"
+                                    userID:@"manager"
+                                    userName:@"manager"
+                                    isOwn:NO];
 
     PEPIdentity *identityMember1 = [[PEPIdentity alloc]
                                     initWithAddress:@"member1@pep.security"
@@ -70,7 +70,7 @@
         XCTAssertNil(error);
     }
 
-    for (PEPIdentity *ident in @[identyManager, identityMember1]) {
+    for (PEPIdentity *ident in @[identityManager, identityMember1]) {
         error = nil;
         XCTAssertTrue([session updateIdentity:ident error:&error]);
         XCTAssertNil(error);
@@ -79,7 +79,7 @@
     error = nil;
 
     PEPGroup *group = [session groupCreateGroupIdentity:identityGroup
-                                        managerIdentity:identyManager
+                                        managerIdentity:identityManager
                                        memberIdentities:@[identityMember1]
                                                   error:&error];
 
