428a610490ab8afa8c98ad26a3220c29ed812dd4
Dirk Zimmermann <dz@pep.security>
2021-04-15 12:58:32 +0200

    IOSAD-201 Start PEPSession testGroupRating

--- pEpObjCTests/PEPSessionGroupTest.m
+++ pEpObjCTests/PEPSessionGroupTest.m
@@ -325,4 +325,58 @@
     XCTAssertNil(error);
 }
 
+- (void)testGroupRating
+{
+    PEPIdentity *identityGroup = [[PEPIdentity alloc]
+                                  initWithAddress:@"group@pep.security"
+                                  userID:@"group"
+                                  userName:@"group"
+                                  isOwn:YES];
+
+    PEPIdentity *identityManager = [[PEPIdentity alloc]
+                                    initWithAddress:@"manager@pep.security"
+                                    userID:@"manager"
+                                    userName:@"manager"
+                                    isOwn:NO];
+
+    PEPIdentity *identityMember1 = [[PEPIdentity alloc]
+                                    initWithAddress:@"member1@pep.security"
+                                    userID:@"member1"
+                                    userName:@"member1"
+                                    isOwn:NO];
+
+    NSError *error = nil;
+
+    for (PEPIdentity *ident in @[identityGroup]) {
+        error = nil;
+        XCTAssertTrue([self mySelf:ident error:&error]);
+        XCTAssertNil(error);
+    }
+
+    for (PEPIdentity *ident in @[identityManager, identityMember1]) {
+        error = nil;
+        XCTAssertTrue([self updateIdentity:ident error:&error]);
+        XCTAssertNil(error);
+    }
+
+    error = nil;
+
+    PEPGroup *group = [self groupCreateGroupIdentity:identityGroup
+                                     managerIdentity:identityManager
+                                    memberIdentities:@[identityMember1]
+                                               error:&error];
+
+    XCTAssertNotNil(group);
+    XCTAssertNil(error);
+
+    XCTAssertNotNil(group);
+
+    error = nil;
+    NSNumber *ratingNumber = [self groupRatingGroupIdentity:identityGroup
+                                            managerIdentity:identityManager
+                                                      error:&error];
+    XCTAssertNotNil(ratingNumber);
+    XCTAssertNil(error);
+}
+
 @end
