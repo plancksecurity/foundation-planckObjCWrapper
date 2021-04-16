e56d1772e54de2d90f87267002e9779d8d1ffc18
Dirk Zimmermann <dz@pep.security>
2021-04-16 08:47:28 +0200

    IOSAD-201 Generic archive test util

---
+++ PEPObjCAdapterTypesTests_iOS/TestUtils/XCTestCase+Archive.h
@@ -0,0 +1,19 @@
+//
+//  XCTestCase+Archive.h
+//  PEPObjCAdapterTypesTests_iOS
+//
+//  Created by Dirk Zimmermann on 16.04.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XCTestCase (Archive)
+
++ (NSObject *)archiveAndUnarchiveObject:(NSObject *)object ofClass:(Class)ofClass;
+
+@end
+
+NS_ASSUME_NONNULL_END
