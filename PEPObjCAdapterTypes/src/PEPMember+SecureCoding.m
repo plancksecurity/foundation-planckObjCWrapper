10910de062e12a8982f8a080b4f80262ae64e589
Dirk Zimmermann <dz@pep.security>
2021-04-16 13:34:53 +0200

    IOSAD-201 PEPMember+SecureCoding

---
+++ PEPObjCAdapterTypes/src/PEPMember+SecureCoding.m
@@ -0,0 +1,13 @@
+//
+//  PEPMember+SecureCoding.m
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 16.04.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import "PEPMember+SecureCoding.h"
+
+@implementation PEPMember (SecureCoding)
+
+@end
