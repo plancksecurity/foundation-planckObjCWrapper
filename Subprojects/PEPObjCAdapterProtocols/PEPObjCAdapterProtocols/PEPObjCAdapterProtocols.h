9be50e2069dc1970e8d75f2069ca1eaf1d5dcb41
David Alarcon <momachilles@gmail.com>
2021-06-21 16:23:47 +0200

    IOSAD-207 Create PEPObjCAdapterProtocols project.

---
+++ Subprojects/PEPObjCAdapterProtocols/PEPObjCAdapterProtocols/PEPObjCAdapterProtocols.h
@@ -0,0 +1,12 @@
+//
+//  PEPObjCAdapterProtocols.h
+//  PEPObjCAdapterProtocols
+//
+//  Created by David Alarcon on 21/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PEPObjCAdapterProtocols : NSObject
+
+@end
