675f9e4182ce0141e3c7640c3e20e81c8c0705b5
David Alarcon <momachilles@gmail.com>
2021-06-22 10:28:48 +0200

    IOSAD-207 Add /PEPObjCAdapterProtocols_iOS target.

---
+++ Subprojects/PEPObjCAdapterProtocols/PEPObjCAdapterProtocols_iOS/PEPObjCAdapterProtocols_iOS.h
@@ -0,0 +1,12 @@
+//
+//  PEPObjCAdapterProtocols_iOS.h
+//  PEPObjCAdapterProtocols_iOS
+//
+//  Created by David Alarcon on 22/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PEPObjCAdapterProtocols_iOS : NSObject
+
+@end
---
+++ Subprojects/PEPObjCAdapterProtocols/PEPObjCAdapterProtocols_iOS/PEPObjCAdapterProtocols_iOS.m
@@ -0,0 +1,12 @@
+//
+//  PEPObjCAdapterProtocols_iOS.m
+//  PEPObjCAdapterProtocols_iOS
+//
+//  Created by David Alarcon on 22/6/21.
+//
+
+#import "PEPObjCAdapterProtocols_iOS.h"
+
+@implementation PEPObjCAdapterProtocols_iOS
+
+@end
