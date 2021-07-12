a605d19effb527ab2095185af9eb3769a05a6878
Andreas Buff <andreas@pep-security.net>
2021-07-12 11:14:21 +0200

    IOSAD-213 PEPObjCTypeUtils is now a subproject

---
+++ Subprojects/PEPObjCTypeUtils/PEPObjCTypeUtils/Interface/PEPObjCTypeUtils.h
@@ -0,0 +1,15 @@
+//
+//  PEPObjCTypeUtils.h
+//  PEPObjCTypeUtils
+//
+//  Created by Andreas Buff on 09.07.21.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPMessage+Engine.h"
+#import "PEPIdentity+Engine.h"
+#import "NSArray+Engine.h"
+@interface PEPObjCTypeUtils : NSObject
+
+@end
