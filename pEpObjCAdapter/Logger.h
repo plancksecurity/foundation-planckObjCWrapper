cb48f6c0c7cf47df4d9cdbba796cc7b820aa96ad
Andreas Buff <andreas@pep.security>
2021-09-13 11:02:52 +0200

    IPS-5 new adapter target for ipsec. Defines NO_TOOLBOX to not depend on iOSToolbox and Lumberjack

--- pEpObjCAdapter/Logger.h
+++ pEpObjCAdapter/Logger.h
@@ -10,11 +10,61 @@
 #define Logger_h
 
 #ifdef IS_IOS_BUILD
-#import <pEpIOSToolboxForExtensions/pEpIOSToolboxForExtensions-Swift.h>
-#import <pEpIOSToolboxForExtensions/PEPLogger.h>
+    #import <pEpIOSToolboxForExtensions/pEpIOSToolboxForExtensions-Swift.h>
+    #import <pEpIOSToolboxForExtensions/PEPLogger.h>
 #else
-#import "PEPToolbox_macOS-Swift.h"
-#import "PEPLogger.h"
+    #ifndef NO_TOOLBOX
+        #import "PEPToolbox_macOS-Swift.h"
+        #import "PEPLogger.h"
+    #endif
 #endif
 
+
+#ifdef NO_TOOLBOX
+/// This is a copy of the iOSToolbox PEPLogger.h interface. Providing a iOSToolbox independend
+/// logging impl.We must not use (at least currently) the iOS toolbox for Linux builds.
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Logger : NSObject
++ (void)logInfoFilename:(const char *)filename
+               function:(const char *)function
+                   line:(NSInteger)line
+                 message:(NSString *)message;
+
++ (void)logWarnFilename:(const char *)filename
+               function:(const char *)function
+                   line:(NSInteger)line
+                message:(NSString *)message;
+
++ (void)logErrorFilename:(const char *)filename
+                function:(const char *)function
+                    line:(NSInteger)line
+                 message:(NSString *)message;
+
++ (void)logErrorAndCrashFilename:(const char *)filename
+                        function:(const char *)function
+                            line:(NSInteger)line
+                         message:(NSString *)message;
+@end
+
+#define LogInfo(...) [Logger logInfoFilename:__FILE__ function:__FUNCTION__ line:__LINE__ message:[NSString stringWithFormat:__VA_ARGS__]];
+
+#define LogWarn(...) [Logger logWarnFilename:__FILE__ function:__FUNCTION__ line:__LINE__ message:[NSString stringWithFormat:__VA_ARGS__]];
+
+#define LogError(...) [Logger logErrorFilename:__FILE__ function:__FUNCTION__ line:__LINE__ message:[NSString stringWithFormat:__VA_ARGS__]];
+
+#define LogError(...) [Logger logErrorFilename:__FILE__ function:__FUNCTION__ line:__LINE__ message:[NSString stringWithFormat:__VA_ARGS__]];
+
+#define LogErrorAndCrash(...) [Logger logErrorAndCrashFilename:__FILE__ function:__FUNCTION__ line:__LINE__ message:[NSString stringWithFormat:__VA_ARGS__]];
+
+NS_ASSUME_NONNULL_END
+
+#endif
+
+
+
 #endif /* Logger_h */
+
+
