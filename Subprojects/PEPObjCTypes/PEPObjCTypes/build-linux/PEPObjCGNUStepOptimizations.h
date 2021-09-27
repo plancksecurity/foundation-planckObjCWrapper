e56c055600eb4c5355a2cd5de0a91f74a68414c9
Andreas Buff <andreas@pep.security>
2021-09-27 15:30:23 +0200

    IPS-5 Finally builds and links.

--- Subprojects/PEPObjCTypes/PEPObjCTypes/build-linux/PEPObjCGNUStepOptimizations.h
+++ Subprojects/PEPObjCTypes/PEPObjCTypes/build-linux/PEPObjCGNUStepOptimizations.h
@@ -1,12 +1,24 @@
 #define UNSUPPORTED_NS_CLOSED_ENUM 1
 #define NO_TOOLBOX 1
+#define NO_MAC_TYPES 1
 
 #ifndef PEP_OBJC_GNUSTEP_OPTIMIZATIONS_H
 #define PEP_OBJC_GNUSTEP_OPTIMIZATIONS_H
 
-/// NS_CLOSED_ENUM is too new for GNUStep ObjC. We replace it with NS_ENUM.
+// NS_CLOSED_ENUM is too new for GNUStep ObjC. We replace it with NS_ENUM.
 #ifdef UNSUPPORTED_NS_CLOSED_ENUM
 #define NS_CLOSED_ENUM NS_ENUM
+#endif
+
+#endif
+
+// Missing types originally defined in MacTypes.h
+#ifndef PEP_OBJC_GNUSTEP_OPTIMIZATIONS_MAC_TYPES_H
+#define PEP_OBJC_GNUSTEP_OPTIMIZATIONS_MAC_TYPES_H
+
+#ifdef NO_MAC_TYPES
+typedef long 						Size;
+typedef unsigned short 				UInt16;
 #endif
 
 #endif
