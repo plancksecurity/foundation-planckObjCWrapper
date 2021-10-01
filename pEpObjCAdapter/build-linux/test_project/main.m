742d3824ead2c0cc5e1757f87f6933c5821954f5
Andreas Buff <andreas@pep.security>
2021-10-01 14:19:56 +0200

    IPS-5 builds incl compiling PCH, without adapter involved

--- pEpObjCAdapter/build-linux/test_project/main.m
+++ pEpObjCAdapter/build-linux/test_project/main.m
@@ -9,9 +9,9 @@
 
 #import "Connector.h"
 #import "HTTPClient.h"
-#import <PEPSession.h>
-#import <PEPMessage.h>
-#import <PEPIdentity.h>
+// #import <PEPSession.h>
+// #import <PEPMessage.h>
+// #import <PEPIdentity.h>
 
 @class MyClass;
 
@@ -21,35 +21,35 @@ static MyClass *s_myClass;
 
 #import "MyClass.h"
 
-void test_using_objc_adapter() {
-    NSString *ownUserID = @"s_ownUserID";
-    NSString *ownAddress = @"me@ownAddress.com";
-    NSString *ownUserName = @"My Name";
+// void test_using_objc_adapter() {
+//     NSString *ownUserID = @"s_ownUserID";
+//     NSString *ownAddress = @"me@ownAddress.com";
+//     NSString *ownUserName = @"My Name";
 
-    PEPMessage *srcMsg = [PEPMessage new];
-    srcMsg.from = me;
-    srcMsg.to = [me];
+//     PEPMessage *srcMsg = [PEPMessage new];
+//     srcMsg.from = me;
+//     srcMsg.to = [me];
 
-    PEPIdentity *me = [[PEPIdentity alloc] initWithAddress:ownUserID
-                                                    userID:ownUserID
-                                                  userName:ownUserName
-                                                     isOwn:YES
-                                               fingerPrint:nil
-                                                  commType:NULL
-                                                  language:nil];
-    PEPSession *session = [PEPSession new];
-    [session mySelf:me errorCallback:^(NSError * _Nonnull error) {
-        NSLog(@"Error: %@", error);
-    } successCallback:^(PEPIdentity * _Nonnull identity) {
-        [session encryptMessage:srcMsg extraKeys:(PEPStringList * _Nullable) errorCallback:^(NSError * _Nonnull error) {
-            NSLog(@"Error: %@", error);
-        } successCallback:^(PEPMessage * _Nonnull srcMessage, PEPMessage * _Nonnull destMessage) {
-            NSLog(@"Success!");
-            NSLog(@"Encrypted message: %@", destMessage);
-            NSLog(@"Original message: %@", srcMessage);
-        }]]
-    }];
-}
+//     PEPIdentity *me = [[PEPIdentity alloc] initWithAddress:ownUserID
+//                                                     userID:ownUserID
+//                                                   userName:ownUserName
+//                                                      isOwn:YES
+//                                                fingerPrint:nil
+//                                                   commType:NULL
+//                                                   language:nil];
+//     PEPSession *session = [PEPSession new];
+//     [session mySelf:me errorCallback:^(NSError * _Nonnull error) {
+//         NSLog(@"Error: %@", error);
+//     } successCallback:^(PEPIdentity * _Nonnull identity) {
+//         [session encryptMessage:srcMsg extraKeys:(PEPStringList * _Nullable) errorCallback:^(NSError * _Nonnull error) {
+//             NSLog(@"Error: %@", error);
+//         } successCallback:^(PEPMessage * _Nonnull srcMessage, PEPMessage * _Nonnull destMessage) {
+//             NSLog(@"Success!");
+//             NSLog(@"Encrypted message: %@", destMessage);
+//             NSLog(@"Original message: %@", srcMessage);
+//         }]]
+//     }];
+// }
 
 void test_arc_dealloc_once(NSString *baseName)
 {
@@ -111,7 +111,7 @@ int main(int argc, const char * argv[])
         test_arc_dealloc();
         test_stream_connection();
         request();
-        test_using_objc_adapter()
+        // test_using_objc_adapter()
     }
 
     return 0;
