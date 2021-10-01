206cc5921182b3508a4eb36cc6c10de03228e2df
Andreas Buff <andreas@pep.security>
2021-10-01 11:48:20 +0200

    IPS-5 adds adapter usage test skeleton

--- pEpObjCAdapter/build-linux/test_project/main.m
+++ pEpObjCAdapter/build-linux/test_project/main.m
@@ -9,12 +9,47 @@
 
 #import "Connector.h"
 #import "HTTPClient.h"
+#import <PEPSession.h>
+#import <PEPMessage.h>
+#import <PEPIdentity.h>
 
 @class MyClass;
 
 static MyClass *s_myClass;
 
+
+
 #import "MyClass.h"
+
+void test_using_objc_adapter() {
+    NSString *ownUserID = @"s_ownUserID";
+    NSString *ownAddress = @"me@ownAddress.com";
+    NSString *ownUserName = @"My Name";
+
+    PEPMessage *srcMsg = [PEPMessage new];
+    srcMsg.from = me;
+    srcMsg.to = [me];
+
+    PEPIdentity *me = [[PEPIdentity alloc] initWithAddress:ownUserID
+                                                    userID:ownUserID
+                                                  userName:ownUserName
+                                                     isOwn:YES
+                                               fingerPrint:nil
+                                                  commType:NULL
+                                                  language:nil];
+    PEPSession *session = [PEPSession new];
+    [session mySelf:me errorCallback:^(NSError * _Nonnull error) {
+        NSLog(@"Error: %@", error);
+    } successCallback:^(PEPIdentity * _Nonnull identity) {
+        [session encryptMessage:srcMsg extraKeys:(PEPStringList * _Nullable) errorCallback:^(NSError * _Nonnull error) {
+            NSLog(@"Error: %@", error);
+        } successCallback:^(PEPMessage * _Nonnull srcMessage, PEPMessage * _Nonnull destMessage) {
+            NSLog(@"Success!");
+            NSLog(@"Encrypted message: %@", destMessage);
+            NSLog(@"Original message: %@", srcMessage);
+        }]]
+    }];
+}
 
 void test_arc_dealloc_once(NSString *baseName)
 {
@@ -76,6 +111,7 @@ int main(int argc, const char * argv[])
         test_arc_dealloc();
         test_stream_connection();
         request();
+        test_using_objc_adapter()
     }
 
     return 0;
