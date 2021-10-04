109c5bf5e64810f027049a0828c28212861fd8a5
Andreas Buff <andreas@pep.security>
2021-10-04 15:04:03 +0200

    IPS-5 sqlite3 build

---
+++ Subprojects/PEPObjCTypeUtils/PEPObjCTypeUtils/build-linux/build-sqlite3.sh
@@ -0,0 +1,58 @@
+SCRIPT_DIR=$(dirname "$0")
+PREFIX=$1
+SRC_DIR=$2
+CURRENT_DIR=$(pwd)
+
+# Get Absolute Paths & Setup
+
+cd "${SCRIPT_DIR}"
+    SCRIPT_DIR=$(pwd)
+cd "${CURRENT_DIR}"
+if [ "${PREFIX}" = "" ]; then
+    PREFIX=~/local
+fi
+mkdir -p "${PREFIX}"
+cd "${PREFIX}"
+    PREFIX=$(pwd)
+cd "${CURRENT_DIR}"
+if [ "${SRC_DIR}" = "" ]; then
+    SRC_DIR=~/src
+fi
+mkdir -p "${SRC_DIR}"
+cd "${SRC_DIR}"
+    SRC_DIR=$(pwd)
+cd "${CURRENT_DIR}"
+
+LIB_DIR="${PREFIX}/lib"
+mkdir -p "${LIB_DIR}"
+
+SQLITE_VERSION="3360000"
+SQLITE_NAME="sqlite-autoconf-${SQLITE_VERSION}"
+
+# Exit on errors
+set -e
+
+if [ -f "${LIB_DIR}/libsqlite3.a" ]; then
+    echo "lib exists already in ${LIB_DIR}. If you want to rebuild it, delete the existing one."
+    exit 0
+fi
+
+SQLITE_DIR="${SRC_DIR}/${SQLITE_NAME}"
+if [ ! -d "${SQLITE_DIR}" ]; then
+    cd "${SRC_DIR}"
+        wget -nc https://www.sqlite.org/2021/sqlite-autoconf-3360000.tar.gz
+        tar xvf ${SQLITE_NAME}.tar.gz
+    cd "${CURRENT_DIR}"
+fi
+
+cd "${SRC_DIR}/${SQLITE_NAME}/"
+    gcc -c sqlite3.c -o sqlite3.o
+    ar qf libsqlite3.a sqlite3.o
+    ranlib libsqlite3.a
+    mv libsqlite3.a "${PREFIX}/lib"
+    mv sqlite3.h "${PREFIX}/include"
+cd "${CURRENT_DIR}"
+
+rm -rf "${SRC_DIR}"*.gz
+rm -rf "${LIB_DIR}/"*.so*
+rm -rf "${LIB_DIR}/"*.la
