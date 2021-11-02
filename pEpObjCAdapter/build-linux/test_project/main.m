b62361db5d4510578ec220758c3c98f630eb8ff2
Andreas Buff <ab@pep.security>
2021-11-02 16:41:52 +0100

    IPS-5_experiment_dipatch_main_queue_deadlock rm clutter

--- pEpObjCAdapter/build-linux/test_project/main.m
+++ pEpObjCAdapter/build-linux/test_project/main.m
@@ -7,69 +7,6 @@
 
 #import <Foundation/Foundation.h>
 
-#import "Connector.h"
-#import "HTTPClient.h"
-#import <PEPSession.h>
-#import <PEPMessage.h>
-#import <PEPIdentity.h>
-
-@class MyClass;
-
-static MyClass *s_myClass;
-
-
-
-#import "MyClass.h"
-
-void test_arc_dealloc_once(NSString *baseName)
-{
-    NSLog(@"%@: test_arc_dealloc", baseName);
-    NSArray *objs = @[[[MyClass alloc]
-                       initWithName:[NSString stringWithFormat:@"%@_%@",
-                                     baseName, @"1"]],
-                      [[MyClass alloc]
-                       initWithName:[NSString stringWithFormat:@"%@_%@",
-                                     baseName, @"2"]],
-                      [[MyClass alloc]
-                       initWithName:[NSString stringWithFormat:@"%@_%@",
-                                     baseName, @"3"]]];
-    for (MyClass *obj in objs) {
-        NSLog(@"%@: Have object: %@", baseName, obj.name);
-    }
-
-    // Finalize all objects
-    NSLog(@"%@: Set objs to nil.", baseName);
-    objs = nil;
-}
-
-void test_arc_dealloc(void)
-{
-    dispatch_group_t group = dispatch_group_create();
-
-    dispatch_queue_t queue = dispatch_queue_create("1", NULL);
-
-    for (NSNumber *num in @[@1, @2, @3, @4, @5, @6, @7, @8, @9]) {
-        dispatch_group_async(group, queue, ^{
-            test_arc_dealloc_once([NSString stringWithFormat:@"base_%@", num]);
-        });
-    }
-
-    dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
-}
-
-void test_stream_connection(void)
-{
-    Connector *connector = [Connector new];
-    [connector connectWithHostname:@"pep.security" port:80];
-}
-
-void request(void)
-{
-    HTTPClient *httpClient = [HTTPClient new];
-    NSData *data = [httpClient requestURLString:@"https://www.pep.security/en/"];
-    NSLog(@"Received %lu bytes", (unsigned long) data.length);
-}
-
 void test_dispatchToMainQueueNeverExecuted() {
     NSLog(@"test_dispatchToMainQueueNeverExecuted: Started");
     dispatch_async(dispatch_get_global_queue(0, 0), ^{
@@ -82,70 +19,12 @@ void test_dispatchToMainQueueNeverExecuted() {
 
 }
 
-void test_using_objc_adapter() {
-    NSLog(@"Starting: test_using_objc_adapter");
-    NSString *ownUserID = @"s_ownUserID";
-    NSString *ownAddress = @"me@ownAddress.com";
-    NSString *ownUserName = @"My Name";
-
-    PEPIdentity *me = [[PEPIdentity alloc] initWithAddress:ownAddress
-                                                    userID:ownUserID
-                                                  userName:ownUserName
-                                                     isOwn:YES
-                                               fingerPrint:nil
-                                                  commType:PEPCommTypeUnknown
-                                                  language:nil];
-
-    PEPMessage *srcMsg = [PEPMessage new];
-    srcMsg.from = me;
-    srcMsg.to = @[me];
-
-    dispatch_group_t group = dispatch_group_create();
-    dispatch_group_enter(group);
-    NSLog(@"Go Background");
-    dispatch_async(dispatch_get_global_queue(0, 0), ^{
-        NSLog(@"test_using_objc_adapter: call myself");
-        PEPSession *session = [PEPSession new];
-        [session mySelf:me errorCallback:^(NSError * _Nonnull error) {
-            NSLog(@"Error: %@", error);
-            dispatch_group_leave(group);
-        } successCallback:^(PEPIdentity * _Nonnull identity) {
-            // PEPSession *session = [PEPSession new];
-            // [session encryptMessage:srcMsg extraKeys:nil errorCallback:^(NSError * _Nonnull error) {
-            //     NSLog(@"Error: %@", error);
-            //     dispatch_group_leave(group);
-            // } successCallback:^(PEPMessage * _Nonnull srcMessage, PEPMessage * _Nonnull destMessage) {
-            //     NSLog(@"Success!");
-            //     NSLog(@"Encrypted message: %@", destMessage);
-            //     NSLog(@"Original message: %@", srcMessage);
-
-            //     NSLog(@"Finnished_using_objc_adapter");
-            //     dispatch_group_leave(group);
-            // }];
-
-            dispatch_group_leave(group);
-        }];
-    });
-    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
-        NSLog(@"Done");
-    });
-    // dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
-    NSLog(@"test_using_objc_adapter: waitng for adapter to return");
-}
-
 int main(int argc, const char * argv[])
 {
     @autoreleasepool {
         NSLog(@"main");
-            // s_myClass = [[MyClass alloc] initWithName:@"static"];
-            // s_myClass = nil;
-
-            // test_arc_dealloc();
-            // test_stream_connection();
-            // request();
-
-        //test_using_objc_adapter();
-        test_dispatchToMainQueueNeverExecuted()
+           
+        test_dispatchToMainQueueNeverExecuted();
 
         // NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
         // NSLog(@"runLoop: %@", runLoop);
