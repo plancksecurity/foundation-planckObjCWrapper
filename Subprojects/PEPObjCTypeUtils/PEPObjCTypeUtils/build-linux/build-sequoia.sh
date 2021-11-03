aaf8249acf4854baa890fcb2f1bb7159572ea01f
Andreas Buff <ab@pep.security>
2021-11-03 15:52:12 +0100

    IPS-5 rm commented stuff

--- Subprojects/PEPObjCTypeUtils/PEPObjCTypeUtils/build-linux/build-sequoia.sh
+++ Subprojects/PEPObjCTypeUtils/PEPObjCTypeUtils/build-linux/build-sequoia.sh
@@ -43,53 +43,10 @@ if [ ! -d "${SEQUOIA_DIR}" ]; then
 fi
 
 cd "${SEQUOIA_DIR}"
-    #PKG_CONFIG_ALLOW_CROSS=1 RUST_BACKTRACE=full PKG_CONFIG_PATH=${CURRENT_LIB_DIR}/lib/pkgconfig RUSTFLAGS="-L ${PREFIX}/lib" cargo build --target ${HOST} --release
-    #RUSTFLAGS="-L ${LIB_DIR}" cargo build --release
     cargo update
     RUSTFLAGS="-L ${LIB_DIR}" cargo build -p sequoia-openpgp-ffi --release
     make -C openpgp-ffi install PREFIX=${PREFIX}
 cd "${CURRENT_DIR}"
-
-
-#cp "${SEQUOIA_DIR}/target/${HOST}/release"/*.a "${CURRENT_LIB_DIR}/lib"
-
-# # copy headers
-# cp -R "${SEQUOIA_DIR}/openpgp-ffi/include"/* "${CURRENT_LIB_DIR}/include/"
-#     # END: Lib Specific Code
-# }
-
-# # Cleanup
-# cleanup()
-# {
-#     rm -rf "${TMP_DIR}/x86_64"
-#     rm -rf "${TMP_DIR}/arm64"
-#     export MACOSX_DEPLOYMENT_TARGET=10.10
-# }
-# cleanup
-
-# # Setup
-
-# mkdir -p "${TMP_DIR}"
-# mkdir -p "${PREFIX}"
-
-# # Build
-# pushd "${TMP_DIR}"
-#     buildLib "x86_64" "x86_64-apple-macos10.10" "x86_64-apple-darwin" "macosx"
-#     buildLib "arm64" "arm64-apple-macos11.1" "aarch64-apple-darwin" "macosx"
-# popd
-
-# # Make Fat Lib
-# sh "${SCRIPT_DIR}/build-fat-libs.sh" "${TMP_DIR}/arm64/lib" "${TMP_DIR}/x86_64/lib" "${LIB_DIR}"
-# # Make Fat Bin
-# sh "${SCRIPT_DIR}/build-fat-bins.sh" "${TMP_DIR}/arm64/bin" "${TMP_DIR}/x86_64/bin" "${BIN_DIR}"
-
-
-# # Copy Headers If Currently Building Lib
-# ARM64_INCLUDE_DIR="${TMP_DIR}/arm64/include"
-# cp -r "${ARM64_INCLUDE_DIR}"/* "${INCLUDE_DIR}" 2>/dev/null || :
-
-# # Cleanup
-# cleanup
 
 rm -rf "${SRC_DIR}"
 rm -rf "${LIB_DIR}/"*.so*
