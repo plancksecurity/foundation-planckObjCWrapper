b9e6c679ffa2a1547c1fbb2a10ff95dff6843699
Dirk Zimmermann <dz@pep.security>
2021-11-08 11:55:10 +0100

    IPS-9 Move PEPMessage+ASN1

---
+++ Subprojects/PEPObjCTypeUtils/PEPObjCTypeUtils/PEPMessage+ASN1.m
@@ -0,0 +1,55 @@
+//
+//  PEPMessage+ASN1.m
+//  pEpCC_macOS
+//
+//  Created by Dirk Zimmermann on 25.08.21.
+//
+
+#import "PEPMessage+ASN1.h"
+
+#import "PEPObjCTypeUtils.h"
+#import "map_asn1.h"
+#import "message_codec.h"
+
+@implementation PEPMessage (ASN1)
+
++ (instancetype _Nullable)messageFromAsn1Data:(NSData *)asn1Data
+{
+    ASN1Message_t *asn1Message = NULL;
+    PEP_STATUS status = decode_ASN1Message_message(asn1Data.bytes, asn1Data.length, &asn1Message);
+    if (status != PEP_STATUS_OK) {
+        return nil;
+    }
+
+    message *msg = ASN1Message_to_message(asn1Message, NULL, YES, 0);
+    if (!msg) {
+        return nil;
+    }
+
+    return [PEPMessage fromStruct:msg];
+}
+
+- (NSData *)asn1Data
+{
+    message *msg = [self toStruct];
+    ASN1Message_t *asn1Message = ASN1Message_from_message(msg, NULL, YES, 0);
+    free_message(msg);
+
+    if (asn1Message == NULL) {
+        return nil;
+    }
+
+    char *msgBytes = NULL;
+    size_t msgBytesSze = 0;
+    PEP_STATUS status = encode_ASN1Message_message(asn1Message, &msgBytes, &msgBytesSze);
+
+    if (status != PEP_STATUS_OK) {
+        return nil;
+    }
+
+    NSData *msgData = [[NSData alloc] initWithBytesNoCopy:msgBytes length:msgBytesSze];
+
+    return msgData;
+}
+
+@end
---
+++ Subprojects/PEPObjCTypeUtils/PEPObjCTypeUtils/PublicHeaders/PEPMessage+ASN1.h
@@ -0,0 +1,30 @@
+//
+//  PEPMessage+ASN1.h
+//  pEpCC_macOS
+//
+//  Created by Dirk Zimmermann on 25.08.21.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPMessage.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Extensions for converting asn.1 to and from `PEPMessage`.
+///
+/// @see https://dev.pep.foundation/Engine/ASN1Message
+@interface PEPMessage (ASN1)
+
+/// Try to create a message from bytes, using asn.1.
++ (instancetype _Nullable)messageFromAsn1Data:(NSData *)asn1Data;
+
+/// The message serialized to bytes, via asn.1.
+///
+/// @note All `PEPIdentities` must have a fingerprint, or the encoding will fail.
+/// @return The asn.1 encoded message bytes, or nil if encoding didn't work, and maybe on insufficient memory.
+- (NSData * _Nullable)asn1Data;
+
+@end
+
+NS_ASSUME_NONNULL_END
