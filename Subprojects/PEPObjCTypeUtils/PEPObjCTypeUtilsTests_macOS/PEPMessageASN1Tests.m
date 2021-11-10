466913b675a1a5583a6e3109b5d14b5ddff8713a
Dirk Zimmermann <dz@pep.security>
2021-11-10 13:12:42 +0100

    IPS-9 Remove all ENGINE-970 related test code

--- Subprojects/PEPObjCTypeUtils/PEPObjCTypeUtilsTests_macOS/PEPMessageASN1Tests.m
+++ Subprojects/PEPObjCTypeUtils/PEPObjCTypeUtilsTests_macOS/PEPMessageASN1Tests.m
@@ -109,50 +109,6 @@
     XCTAssertEqualObjects(msg1, msg2);
 }
 
-// TODO: Re-enable with ENGINE-970 having been fixed.
-- (void)testBasicAsn1EncodeDecodeRoundTripMessageWithAttachment
-{
-    PEPIdentity *fromId = [[PEPIdentity alloc]
-                           initWithAddress:@"someone1@example.com"
-                           userID:@"1"
-                           userName:@"Some One 1"
-                           isOwn:NO];
-    fromId.fingerPrint = @"0E12343434343434343434EAB3484343434343434";
-
-    PEPIdentity *toId = [[PEPIdentity alloc]
-                         initWithAddress:@"someone2@example.com"
-                         userID:@"2"
-                         userName:@"Some One 2"
-                         isOwn:NO];
-    toId.fingerPrint = @"123434343434343C3434343434343734349A34344";
-
-    NSData *randomData = [self randomDataWithLength:5000];
-    PEPAttachment *attachment = [[PEPAttachment alloc]
-                                 initWithData:randomData];
-    attachment.mimeType = @"application/pgp-keys";
-    attachment.filename = @"key.asc";
-
-    PEPMessage *msg1 = [PEPMessage new];
-    msg1.from = fromId;
-    msg1.to = @[toId];
-    msg1.attachments = @[attachment];
-
-    NSData *blob = [msg1 asn1Data];
-    XCTAssertNotNil(blob);
-
-    PEPMessage *msg2 = [PEPMessage messageFromAsn1Data:blob];
-    XCTAssertNotNil(msg2);
-
-    XCTAssertEqualObjects(msg1, msg2);
-    XCTAssertEqual(msg1.attachments.count, msg2.attachments.count);
-}
-
- // TODO: Engine test case. Re-enable with ENGINE-970 having been fixed, verify, and then remove.
-//- (void)testCaseAsnEncodeMessageAttachment
-//{
-//    XCTAssertTrue(testCaseAsnEncodeMessageAttachment());
-//}
-
 #pragma mark - Private Helpers
 
 - (NSData *)randomDataWithLength:(NSUInteger)length
