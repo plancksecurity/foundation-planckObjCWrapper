b3ec65d7c59d965aab8e2aa49113f8b49440dcda
Dirk Zimmermann <dz@pep.security>
2021-11-29 12:33:22 +0100

    IPS-91 Console: Try to get the async right

--- pEpObjCAdapterTestStartup/main.m
+++ pEpObjCAdapterTestStartup/main.m
@@ -14,8 +14,24 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"Hello, World!");
-        PEPSession *session = [PEPSession new];
-        session = nil;
+
+        dispatch_group_t group = dispatch_group_create();
+        dispatch_queue_t queue = dispatch_queue_create("queue", nil);
+
+        dispatch_group_enter(group);
+
+        dispatch_sync(queue, ^{
+            PEPSession *session = [PEPSession new];
+            [session languageList:^(NSError * _Nonnull error) {
+                NSLog(@"*** error: error");
+                dispatch_group_leave(group);
+            } successCallback:^(NSArray<PEPLanguage *> * _Nonnull languages) {
+                NSLog(@"*** have %lu languages", (unsigned long) languages.count);
+                dispatch_group_leave(group);
+            }];
+        });
+
+        dispatch_group_wait(group, durationForever);
     }
     return 0;
 }
