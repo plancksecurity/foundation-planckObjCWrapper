a588ba0815c972b4796bf859db9a816f91fec428
Dirk Zimmermann <dz@pep.security>
2021-11-29 12:18:11 +0100

    IPS-91 Console program to test adapter startup

---
+++ pEpObjCAdapterTestStartup/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  pEpObjCAdapterTestStartup
+//
+//  Created by Dirk Zimmermann on 29.11.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
