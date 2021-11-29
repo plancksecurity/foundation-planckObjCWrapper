9bca79902661d7caf5f79d9191a21a790f89573c
Dirk Zimmermann <dz@pep.security>
2021-11-29 15:49:55 +0100

    IOSAD-225 Consider rating in conversions

--- Subprojects/PEPObjCTypeUtils/PEPObjCTypeUtils/PEPObjCTypeConversionUtil.m
+++ Subprojects/PEPObjCTypeUtils/PEPObjCTypeUtils/PEPObjCTypeConversionUtil.m
@@ -117,6 +117,8 @@
         msg->attachments = [self arrayToBloblist:pEpMessage.attachments];
     }
 
+    msg->rating = (PEP_rating) pEpMessage.rating;
+
     return msg;
 }
 
@@ -194,6 +196,8 @@
     if (message->attachments && message->attachments->value) {
         pEpMessage.attachments = [self arrayFromBloblist:message->attachments];
     }
+
+    pEpMessage.rating = (PEPRating) message->rating;
 }
 
 + (void)removeEmptyRecipientsFromPEPMessage:(PEPMessage *)pEpMessage
@@ -239,6 +243,7 @@
     pEpMessage.keywords = nil;
     pEpMessage.receivedBy = nil;
     pEpMessage.direction = (PEPMsgDirection) PEP_dir_incoming; // basically, 0
+    pEpMessage.rating = PEPRatingUndefined;
 }
 
 // MARK: - PEPIdentity
