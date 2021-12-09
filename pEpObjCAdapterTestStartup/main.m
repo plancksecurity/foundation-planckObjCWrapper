42d0a96acbeec0860a6d09eb06dc96a3f3178ed4
Dirk Zimmermann <dz@pep.security>
2021-12-09 15:55:00 +0100

    IOSAD-228 TMP Revive console app from IPS-91
    This reverts commit 56974065a12387df6f36c7cf3ed7bbf3f10db08f.

---
+++ pEpObjCAdapterTestStartup/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  pEpObjCAdapterTestStartup
+//
+//  Created by Dirk Zimmermann on 29.11.21.
+//  Copyright © 2021 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPObjCAdapter.h"
+#import "PEPSession.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, World!");
+
+        PEPSession *session = [PEPSession new];
+        [session ratingFromString:@"blarg"];
+    }
+    return 0;
+}
