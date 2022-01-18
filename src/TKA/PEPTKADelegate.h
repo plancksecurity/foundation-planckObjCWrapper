f4a75ecee6f68d941023c9a6913aa55757056944
Dirk Zimmermann <dz@pep.security>
2022-01-18 15:22:09 +0100

    IOSAD-233 Gotta have more delegates (for tka)

---
+++ src/TKA/PEPTKADelegate.h
@@ -0,0 +1,17 @@
+//
+//  PEPTKADelegate.h
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 18.01.22.
+//  Copyright © 2022 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPTKADelegate : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ src/TKA/PEPTKADelegate.m
@@ -0,0 +1,13 @@
+//
+//  PEPTKADelegate.m
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 18.01.22.
+//  Copyright © 2022 p≡p. All rights reserved.
+//
+
+#import "PEPTKADelegate.h"
+
+@implementation PEPTKADelegate
+
+@end
