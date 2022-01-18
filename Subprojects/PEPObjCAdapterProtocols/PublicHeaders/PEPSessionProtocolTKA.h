bc57fa77de41d5faecdc8f6b5f09a0afa874d424
Dirk Zimmermann <dz@pep.security>
2022-01-18 16:40:11 +0100

    IOSAD-233 Declare PEPSessionProtocolTKA

--- Subprojects/PEPObjCAdapterProtocols/PublicHeaders/PEPSessionProtocolTKA.h
+++ Subprojects/PEPObjCAdapterProtocols/PublicHeaders/PEPSessionProtocolTKA.h
@@ -7,21 +7,21 @@
 
 #import <Foundation/Foundation.h>
 
+#import "PEPTKADelegate.h"
+
 NS_ASSUME_NONNULL_BEGIN
 
 @protocol PEPSessionProtocolTKA <NSObject>
 
-/*
 /// Wraps the engine's `tka_subscribe_keychange`.
 - (void)tkaSubscribeKeychangeDelegate:(id<PEPTKADelegate>)delegate
                         errorCallback:(void (^)(NSError *error))errorCallback
-                      successCallback:(void (^)());
+                      successCallback:(void (^)(void))successCallback;
 
 /// Wraps the engine's `tka_request_temp_key`.
 - (void)tkaRequestTempKeyMe:(PEPIdentity *)me partner:(PEPIdentity *)partner
               errorCallback:(void (^)(NSError *error))errorCallback
-            successCallback:(void (^)());
-*/
+            successCallback:(void (^)(void))successCallback;
 
 @end
 
