b11856832cf1d07fbda01a8e028af215ee4363c3
Dirk Zimmermann <dz@pep.security>
2022-01-18 16:00:08 +0100

    IOSAD-233 Give the tka protocol (commented out) methods

--- Subprojects/PEPObjCAdapterProtocols/PublicHeaders/PEPSessionProtocolTKA.h
+++ Subprojects/PEPObjCAdapterProtocols/PublicHeaders/PEPSessionProtocolTKA.h
@@ -11,6 +11,18 @@ NS_ASSUME_NONNULL_BEGIN
 
 @protocol PEPSessionProtocolTKA <NSObject>
 
+/*
+/// Wraps the engine's `tka_subscribe_keychange`.
+- (void)tkaSubscribeKeychangeDelegate:(id<PEPTKADelegate>)delegate
+                        errorCallback:(void (^)(NSError *error))errorCallback
+                      successCallback:(void (^)());
+
+/// Wraps the engine's `tka_request_temp_key`.
+- (void)tkaRequestTempKeyMe:(PEPIdentity *)me partner:(PEPIdentity *)partner
+              errorCallback:(void (^)(NSError *error))errorCallback
+            successCallback:(void (^)());
+*/
+
 @end
 
 NS_ASSUME_NONNULL_END
