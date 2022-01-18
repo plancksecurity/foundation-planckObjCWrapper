57898b0bf8db657f17d6d382d036b2f61882e0bd
Dirk Zimmermann <dz@pep.security>
2022-01-18 15:20:15 +0100

    IOSAD-233 Start PEPInternalSession+TKA

---
+++ src/TKA/PEPInternalSession+TKA.h
@@ -0,0 +1,17 @@
+//
+//  PEPInternalSession+TKA.h
+//  PEPObjCAdapter_iOS
+//
+//  Created by Dirk Zimmermann on 18.01.22.
+//  Copyright © 2022 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPInternalSession_TKA : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ src/TKA/PEPInternalSession+TKA.m
@@ -0,0 +1,13 @@
+//
+//  PEPInternalSession+TKA.m
+//  PEPObjCAdapter_iOS
+//
+//  Created by Dirk Zimmermann on 18.01.22.
+//  Copyright © 2022 p≡p. All rights reserved.
+//
+
+#import "PEPInternalSession+TKA.h"
+
+@implementation PEPInternalSession_TKA
+
+@end
