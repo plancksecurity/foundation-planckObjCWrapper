947a4409c446e5a914b6ae6470a518a0886345bb
Dirk Zimmermann <dz@pep.security>
2022-01-18 15:54:23 +0100

    IOSAD-233 Add (empty) PEPSessionProtocolTKA

---
+++ Subprojects/PEPObjCAdapterProtocols/PublicHeaders/PEPSessionProtocolTKA.h
@@ -0,0 +1,16 @@
+//
+//  PEPSessionProtocolTKA.h
+//  PEPObjCAdapterProtocols
+//
+//  Created by Dirk Zimmermann on 18.01.22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol PEPSessionProtocolTKA <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
