786dd3b307159ef7975ad5553cfcc6e54e97e04d
Dirk Zimmermann <dz@pep.security>
2022-01-18 17:33:46 +0100

    IOSAD-233 Fake the engine's tka declarations

--- src/TKA/PEPInternalSession+TKA.m
+++ src/TKA/PEPInternalSession+TKA.m
@@ -10,6 +10,23 @@
 
 #import "PEPStatusNSErrorUtil.h"
 
+typedef PEP_STATUS (*tka_keychange_t)(
+         const pEp_identity *me,
+         const pEp_identity *partner,
+         const char *key
+     );
+
+ PEP_STATUS tka_subscribe_keychange(
+         PEP_SESSION session,
+         tka_keychange_t callback
+     );
+
+ PEP_STATUS tka_request_temp_key(
+         PEP_SESSION session,
+         pEp_identity *me,
+         pEp_identity *partner
+     );
+
 /// The global TKA delegate.
 id<PEPTKADelegate> s_tkaDelegate = nil;
 
