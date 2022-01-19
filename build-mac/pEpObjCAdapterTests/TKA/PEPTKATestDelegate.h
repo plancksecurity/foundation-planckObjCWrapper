cd3df03f7c4dfc4048485f1e496404b56ddcd9b6
Dirk Zimmermann <dz@pep.security>
2022-01-19 11:30:49 +0100

    IOSAD-233 Gotta have a test delegate

---
+++ build-mac/pEpObjCAdapterTests/TKA/PEPTKATestDelegate.h
@@ -0,0 +1,20 @@
+//
+//  PEPTKATestDelegate.h
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 19.01.22.
+//  Copyright © 2022 p≡p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "PEPObjCAdapter.h"
+#import "PEPSessionProtocol.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPTKATestDelegate : NSObject <PEPTKADelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ build-mac/pEpObjCAdapterTests/TKA/PEPTKATestDelegate.m
@@ -0,0 +1,13 @@
+//
+//  PEPTKATestDelegate.m
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 19.01.22.
+//  Copyright © 2022 p≡p. All rights reserved.
+//
+
+#import "PEPTKATestDelegate.h"
+
+@implementation PEPTKATestDelegate
+
+@end
