1b7c6aeda688bf154fd69e7d2313ddfe5658818c
Dirk Zimmermann <dz@pep.security>
2022-01-19 10:21:25 +0100

    IOSAD-233 Take the fake all the way

--- src/TKA/PEPInternalSession+TKA.m
+++ src/TKA/PEPInternalSession+TKA.m
@@ -18,15 +18,23 @@ typedef PEP_STATUS (*tka_keychange_t)(const pEp_identity *me,
                                       const pEp_identity *partner,
                                       const char *key);
 
+tka_keychange_t g_tkaKeyChangeCallback = NULL;
+
 PEP_STATUS tka_subscribe_keychange(PEP_SESSION session,
                                    tka_keychange_t callback) {
-    return PEP_ILLEGAL_VALUE;
+    g_tkaKeyChangeCallback = callback;
+    return PEP_STATUS_OK;
 }
 
 PEP_STATUS tka_request_temp_key(PEP_SESSION session,
                                 pEp_identity *me,
                                 pEp_identity *partner) {
-    return PEP_ILLEGAL_VALUE;
+    if (g_tkaKeyChangeCallback == NULL) {
+        return PEP_ILLEGAL_VALUE;
+    }
+
+    g_tkaKeyChangeCallback(me, partner, "compleeeetely_fake_key");
+    return PEP_STATUS_OK;
 }
 
 // MARK: - Internal Vars and Forward Declarations
