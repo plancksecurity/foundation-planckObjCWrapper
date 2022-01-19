f1d81f2f47e28389d304ed80d8c209b7d4283fda
Dirk Zimmermann <dz@pep.security>
2022-01-19 14:44:32 +0100

    IOSAD-233 Start PEPSessionTKATest

---
+++ build-mac/pEpObjCAdapterTests/TKA/PEPSessionTKATest.m
@@ -0,0 +1,37 @@
+//
+//  PEPSessionTKATest.m
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 19.01.22.
+//  Copyright © 2022 p≡p. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PEPSessionTKATest : XCTestCase
+
+@end
+
+@implementation PEPSessionTKATest
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
