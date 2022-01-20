0cc53cac6e485c3f6c6e7e6a6f654c52b953e4a2
Dirk Zimmermann <dz@pep.security>
2022-01-20 10:47:40 +0100

    IOSAD-233 Use the real TKA from the engine

--- src/TKA/PEPInternalSession+TKA.m
+++ src/TKA/PEPInternalSession+TKA.m
@@ -8,34 +8,11 @@
 
 #import "PEPInternalSession+TKA.h"
 
+#import "tka_api.h"
+
 #import "PEPObjCTypeUtils.h"
 
 #import "PEPStatusNSErrorUtil.h"
-
-// MARK: - Cheap fake of the engine's TKA API
-
-typedef PEP_STATUS (*tka_keychange_t)(const pEp_identity *me,
-                                      const pEp_identity *partner,
-                                      const char *key);
-
-tka_keychange_t g_tkaKeyChangeCallback = NULL;
-
-PEP_STATUS tka_subscribe_keychange(PEP_SESSION session,
-                                   tka_keychange_t callback) {
-    g_tkaKeyChangeCallback = callback;
-    return PEP_STATUS_OK;
-}
-
-PEP_STATUS tka_request_temp_key(PEP_SESSION session,
-                                pEp_identity *me,
-                                pEp_identity *partner) {
-    if (g_tkaKeyChangeCallback == NULL) {
-        return PEP_ILLEGAL_VALUE;
-    }
-
-    g_tkaKeyChangeCallback(me, partner, "compleeeetely_fake_key");
-    return PEP_STATUS_OK;
-}
 
 // MARK: - Internal Vars and Forward Declarations
 
