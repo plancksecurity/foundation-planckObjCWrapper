b09cdb810a05fdc5eaccfb7b0a95fdb005f75ff3
Martin <martin.brude@gmail.com>
2022-02-10 17:45:42 +0100

    IPS-170 - improve docs

--- Subprojects/PEPObjCTypeUtils/src/NSString+Parse.m
+++ Subprojects/PEPObjCTypeUtils/src/NSString+Parse.m
@@ -11,21 +11,22 @@
 
 - (NSString * _Nullable)stringBetweenString:(NSString * _Nonnull)start
                                   andString:(NSString * _Nonnull)end {
-    // Get the range of the separator in the current string
+    // This will obtain the range of the string between strings and then return it.
+    // First, we get the range of the separator in the current string
     NSRange startRange = [self rangeOfString:start];
     if (startRange.location != NSNotFound) {
         NSRange targetRange;
-        //Define the lower bound of the substring, which means just after the separator.
+        //We define the lower bound of the resultant string, which means just after the separator.
         targetRange.location = startRange.location + startRange.length;
-        //The start of the substring will be the total lenght of the string minus the separator location.
+        //The start of the resultant string will be the total lenght of the string minus the separator location.
         targetRange.length = [self length] - targetRange.location;
-        // Get the end separator range.
+        // Then we get the end separator range.
         NSRange endRange = [self rangeOfString:end options:0 range:targetRange];
         if (endRange.location != NSNotFound) {
-            // Update the substring lenght: substract the end separator.
-            // So substring length will be from the start separator up to the end separator.
+            // Update the resultant string lenght: substract the end separator.
+            // So the resultant string length will be from the start separator up to the end separator, both are excluded.
             targetRange.length = endRange.location - targetRange.location;
-            // Extract the substring
+            // Extract the resultant string.
             return [self substringWithRange:targetRange];
         }
     }
