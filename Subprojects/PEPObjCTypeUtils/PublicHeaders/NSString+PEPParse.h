39d1a6bd4fd11daa2e65d44140479c4cc46b6d1b
Martin <martin.brude@gmail.com>
2022-02-11 15:54:43 +0100

    IPS-170 - add some tests

--- Subprojects/PEPObjCTypeUtils/PublicHeaders/NSString+PEPParse.h
+++ Subprojects/PEPObjCTypeUtils/PublicHeaders/NSString+PEPParse.h
@@ -11,15 +11,22 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSString (PEPParse)
 
-/// Retrieve the substring between two strings, if can find it.
-/// Otherwise, will return nil.
+/// Retrieve the first substring between two strings that can be found.
+/// If can't find one or both param will return nil.
+/// If can find the params but there is nothing in between, will return an empty string.
 ///
 /// For example:
-/// ["One two three" stringBetweenString:@"One " andString: @" three"]; it will return @"two".
+/// [@"One two three" stringBetweenString:@"One " andString: @" three"]; it will return @"two".
+/// [@"One 1 three One 2 three One 3 three" stringBetweenString:@"One " andString: @" three"]; it will return @"1".
+/// [@"Onethree" stringBetweenString:@"One " andString: @" three"]; it will return @"".
+/// [@"One three" stringBetweenString:@"Two " andString: @" Four"]; it will return nil.
+/// [@"One three" stringBetweenString:@"One " andString: @" Four"]; it will return nil.
 ///
 /// @param start The start separator. Will not be included in the substring.
 /// @param end The end separator. Will not be included in the substring.
-- (NSString * _Nullable)stringBetweenString:(NSString * _Nonnull)start andString:(NSString * _Nonnull)end;
+///
+/// @return The resultant (sub)string.
+- (NSString * _Nullable)stringBetweenString:(NSString *)start andString:(NSString *)end;
 
 @end
 
--- Subprojects/PEPObjCTypeUtils/build-mac/PEPObjCTypeUtilsTests/NSStringParseTest.m
+++ Subprojects/PEPObjCTypeUtils/build-mac/PEPObjCTypeUtilsTests/NSStringParseTest.m
@@ -34,4 +34,29 @@
     XCTAssertNil(result);
 }
 
+- (void)testStringBetweenStringWithMultipleOccurence {
+    NSString * extractedExpr = [@"One 1 three One 2 three One 3 three" stringBetweenString:@"One " andString: @" three"];
+    NSString *result = extractedExpr;
+    NSString *expectedResult = @"1";
+    XCTAssertTrue([result isEqualToString:expectedResult], @"Strings are not equal %@ %@", result, expectedResult);
+}
+
+- (void)testStringBetweenStringWithNothingInBetween {
+    NSString *result = [@"Onethree" stringBetweenString:@"One" andString: @"three"];
+    NSString *expectedResult = @"";
+    XCTAssertTrue([result isEqualToString:expectedResult], @"Strings are not equal %@ %@", result, expectedResult);
+}
+
+- (void)testStringBetweenStringWithOnlyOneValidParam {
+    NSString *result = [@"Onethree" stringBetweenString:@"One" andString: @"Four"];
+    XCTAssertNil(result);
+}
+
+- (void)testStringBetweenStringWithEmojis {
+    NSString *result = [@"😃😎🤩" stringBetweenString:@"😃" andString: @"🤩"];
+    NSString *expectedResult = @"😎";
+    XCTAssertTrue([result isEqualToString:expectedResult], @"Strings are not equal %@ %@", result, expectedResult);
+}
+
+
 @end
