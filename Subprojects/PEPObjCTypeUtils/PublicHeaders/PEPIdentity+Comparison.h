2cd656c7bfc8a796eccf2b7ae87b97aa6cdd328a
Martin <martin.brude@gmail.com>
2022-02-14 10:43:18 +0100

    IPS-170 - fix grammar

--- Subprojects/PEPObjCTypeUtils/PublicHeaders/PEPIdentity+Comparison.h
+++ Subprojects/PEPObjCTypeUtils/PublicHeaders/PEPIdentity+Comparison.h
@@ -14,7 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /// Determine if the identity passed by param is the same identity.
 ///
-/// @param otherIdentity The identity to compare with.
+/// @param otherIdentity The identity to compare.
 /// @return YES if it's the same identity. Othwerwise it returns NO.
 - (BOOL)isEqualToIdentity:(PEPIdentity *)otherIdentity;
 
