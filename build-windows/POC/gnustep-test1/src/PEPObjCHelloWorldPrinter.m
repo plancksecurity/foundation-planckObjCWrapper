a63927413f06ab66b032415c9e133842cedb6c41
Alex Sualdea <as@pep.security>
2022-02-17 16:12:52 +0100

    IPS-6 POC project files

---
+++ build-windows/POC/gnustep-test1/src/PEPObjCHelloWorldPrinter.m
@@ -0,0 +1,16 @@
+//
+//  PEPObjCHelloWorldPrinter.m
+//  ObjCHelloWorld
+//
+//  Created by Andreas Buff on 20.01.22.
+//
+
+#import "PEPObjCHelloWorldPrinter.h"
+
+@implementation PEPObjCHelloWorldPrinter
+
+- (void)print {
+    NSLog(@"Hello ObjC World!");
+}
+
+@end
---
+++ build-windows/POC/pEpObjCAdapterTest/pEpObjCAdapterTest/src/main.m
@@ -0,0 +1,99 @@
+/// Simple test using pEpObjCAdapter on Linux & GNUstep.
+
+#import <Foundation/Foundation.h>
+
+#import <PEPSession.h>
+#import <PEPMessage.h>
+#import <PEPIdentity.h>
+
+/// Calls myselve and encrypts a message.
+void test_using_objc_adapter() {
+    NSLog(@"test_using_objc_adapter: Starting");
+
+    NSString *ownUserID = @"ownUserID";
+    NSString *ownAddress = @"me@ownAddress.com";
+    NSString *ownUserName = @"My Name";
+
+    PEPIdentity *me = [[PEPIdentity alloc] initWithAddress:ownAddress
+                                                    userID:ownUserID
+                                                  userName:ownUserName
+                                                     isOwn:YES
+                                               fingerPrint:nil
+                                                  commType:PEPCommTypeUnknown
+                                                  language:nil];
+    PEPMessage *srcMsg = [PEPMessage new];
+    srcMsg.from = me;
+    srcMsg.to = @[me];
+    srcMsg.direction = PEPMsgDirectionOutgoing;
+    srcMsg.shortMessage = @"shortMessage";
+    srcMsg.longMessage = @"longMessage";
+    srcMsg.longMessageFormatted = @"longMessageFormatted";
+
+    dispatch_group_t group = dispatch_group_create();
+    dispatch_group_enter(group);
+    
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{ // 0 == DISPATCH_QUEUE_PRORITY_DEFAULT
+        NSLog(@"test_using_objc_adapter: ### call myself ###");
+        PEPSession *session = [PEPSession new];
+        [session mySelf:me errorCallback:^(NSError * _Nonnull error) {
+            NSLog(@"Error: %@", error);
+            dispatch_group_leave(group);
+        } successCallback:^(PEPIdentity * _Nonnull identity) {
+
+            PEPSession *session = [PEPSession new];
+            NSLog(@"test_using_objc_adapter: ### encryptMessage ###");
+            [session encryptMessage:srcMsg extraKeys:nil errorCallback:^(NSError * _Nonnull error) {
+                NSLog(@"test_using_objc_adapter: Error encryptMessage: %@", error);
+                dispatch_group_leave(group);
+            } successCallback:^(PEPMessage * _Nonnull srcMessage, PEPMessage * _Nonnull destMessage) {
+                NSLog(@"test_using_objc_adapter: Encrypted message: %@", destMessage);
+                NSLog(@"test_using_objc_adapter: Original message: %@", srcMessage);
+
+                PEPMessage *encryptedMsg = destMessage;
+                PEPRating rating = PEPRatingUndefined;
+                PEPStringList *keys = nil;
+                NSLog(@"test_using_objc_adapter: ### decryptMessage ###");
+                [session decryptMessage:destMessage flags:NULL extraKeys:keys errorCallback:^(NSError * _Nonnull error) {
+                    NSLog(@"test_using_objc_adapter: Error decryptMessage: %@", error);
+                    dispatch_group_leave(group);
+                } successCallback:^(PEPMessage *decryptSrcMessage, 
+                                    PEPMessage *decryptDstMessage, 
+                                    PEPStringList *keyList, 
+                                    //PEPRating rating,  // AQUI
+                                    PEPDecryptFlags flags,
+                                    BOOL isFormerlyEncryptedReuploadedMessage) {
+                    NSLog(@"test_using_objc_adapter: Decrypted message: %@", decryptDstMessage);
+                    NSLog(@"test_using_objc_adapter: keyList: %@", keyList);
+                    NSLog(@"test_using_objc_adapter: rating: %d", rating);
+                    NSLog(@"test_using_objc_adapter: keyList: %d", flags);
+                    NSLog(@"test_using_objc_adapter: isFormerlyEncryptedReuploadedMessage: %d", isFormerlyEncryptedReuploadedMessage);
+
+                    dispatch_group_leave(group);
+                }];
+
+            }];   
+
+        }];
+    });
+    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+        NSLog(@"test_using_objc_adapter: Done");
+    });
+    NSLog(@"test_using_objc_adapter: waiting for adapter to return");
+}   
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        NSLog(@"main");
+  
+        test_using_objc_adapter();
+        
+        NSLog(@"Entering runloop");
+        NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
+        [runLoop run];
+    }
+
+    NSLog(@"bye!");
+
+    return 0;
+}
