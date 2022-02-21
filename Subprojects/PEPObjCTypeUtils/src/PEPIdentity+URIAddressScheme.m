3647a0e316638b3e92f99adfd2ceeb62e01dca44
Martin <martin.brude@gmail.com>
2022-02-21 10:45:47 +0100

    IPS-193 - move comment

--- Subprojects/PEPObjCTypeUtils/src/PEPIdentity+URIAddressScheme.m
+++ Subprojects/PEPObjCTypeUtils/src/PEPIdentity+URIAddressScheme.m
@@ -60,11 +60,11 @@ NSString *const _Nonnull IPV4Separator = @":";
 }
 
 - (NSString * _Nullable)getProtocol {
+    // As the address scheme for pEp4IPsec is "$PROTOCOL:$IPV4:$PORT" or "$PROTOCOL:[$IPV6]:$PORT",
+    // we can separate the protocol using the IPV4Separator, colon `:`.
     if (![self.address containsString:IPV4Separator]) {
         return nil;
     }
-    // As the address scheme for pEp4IPsec is "$PROTOCOL:$IPV4:$PORT" or "$PROTOCOL:[$IPV6]:$PORT",
-    // we can separate the protocol using the IPV4Separator, colon `:`.
     NSArray *parts = [self.address componentsSeparatedByString:IPV4Separator];
     return [parts firstObject];
 }
