87e7da85d57d1bf00bc3b662fb73bfb7147ec7ed
Dirk Zimmermann <dz@pep.security>
2022-09-06 08:11:59 +0200

    PEMA-24 Assert when restarting existing PEPSync object

--- pEpObjCAdapter/PEPSync.m
+++ pEpObjCAdapter/PEPSync.m
@@ -187,6 +187,9 @@ static __weak PEPSync *s_pEpSync;
 
 - (void)startup
 {
+    // Make it illegal to shutdown sync, and then re-use the object to do a startup again.
+    assert(s_pEpSync);
+
     self.shutdownRequested = NO;
     [self stopWaiting];
 
