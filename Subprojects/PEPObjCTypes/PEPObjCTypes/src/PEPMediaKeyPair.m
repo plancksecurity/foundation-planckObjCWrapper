0fb54f03a3eb9c965d0cc8ba5d58960e87580aa8
Dirk Zimmermann <dz@pep.security>
2022-09-06 11:56:59 +0200

    PEMA-24 PEPMediaKeyPair equal/hash

--- Subprojects/PEPObjCTypes/PEPObjCTypes/src/PEPMediaKeyPair.m
+++ Subprojects/PEPObjCTypes/PEPObjCTypes/src/PEPMediaKeyPair.m
@@ -7,6 +7,8 @@
 
 #import "PEPMediaKeyPair.h"
 
+#import "NSObject+Equality.h"
+
 @implementation PEPMediaKeyPair
 
 - (instancetype)initWithPattern:(NSString *)pattern fingerprint:(NSString *)fingerprint
@@ -17,6 +19,37 @@
         _fingerprint = fingerprint;
     }
     return self;
+}
+
+// MARK: - Equality
+
+- (BOOL)isEqualToPEPMediaKeyPair:(PEPMediaKeyPair * _Nonnull)mediaKeyPair
+{
+    return [self.pattern isEqualToString:mediaKeyPair.pattern] &&
+    [self.fingerprint isEqualToString:mediaKeyPair.fingerprint];
+}
+
+- (NSUInteger)hash
+{
+    NSUInteger prime = 31;
+    NSUInteger result = 1;
+
+    result = prime * result + self.pattern.hash;
+    result = prime * result + self.fingerprint.hash;
+
+    return result;
+}
+
+- (BOOL)isEqual:(id)object
+{
+    if (object == self) {
+        return YES;
+    }
+    if (!object || ![object isKindOfClass:[self class]]) {
+        return NO;
+    }
+
+    return [self isEqualToPEPMediaKeyPair:object];
 }
 
 @end
