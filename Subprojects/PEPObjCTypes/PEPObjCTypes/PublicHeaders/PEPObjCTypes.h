e180bc6060c57c55ae463534b3cd2daaf440dbf7
Dirk Zimmermann <dz@pep.security>
2022-09-06 16:31:59 +0200

    PEMA-24 Add *+SecureCoding.h to the umbrella

--- Subprojects/PEPObjCTypes/PEPObjCTypes/PublicHeaders/PEPObjCTypes.h
+++ Subprojects/PEPObjCTypes/PEPObjCTypes/PublicHeaders/PEPObjCTypes.h
@@ -9,11 +9,21 @@
 
 #import "PEPTypes.h"
 #import "PEPEngineTypes.h"
+
 #import "PEPLanguage.h"
+#import "PEPLanguage+SecureCoding.h"
+
 #import "PEPMessage.h"
+#import "PEPMessage+SecureCoding.h"
+
 #import "PEPAttachment.h"
+#import "PEPAttachment+SecureCoding.h"
+
 #import "PEPIdentity.h"
+#import "PEPIdentity+SecureCoding.h"
+
 #import "PEPMediaKeyPair.h"
+#import "PEPMediaKeyPair+SecureCoding.h"
 
 @interface PEPObjCTypes : NSObject
 
