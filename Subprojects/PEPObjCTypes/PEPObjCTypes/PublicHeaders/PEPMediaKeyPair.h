2dc50e010ed35a4ecc7024048fc153b51f39d99c
Dirk Zimmermann <dz@pep.security>
2022-09-06 08:36:59 +0200

    PEMA-24 Class for media key pair

---
+++ Subprojects/PEPObjCTypes/PEPObjCTypes/PublicHeaders/PEPMediaKeyPair.h
@@ -0,0 +1,16 @@
+//
+//  PEPMediaKeyPair.h
+//  PEPObjCTypes
+//
+//  Created by Dirk Zimmermann on 06.09.22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPMediaKeyPair : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Subprojects/PEPObjCTypes/PEPObjCTypes/src/PEPMediaKeyPair.m
@@ -0,0 +1,12 @@
+//
+//  PEPMediaKeyPair.m
+//  PEPObjCTypes
+//
+//  Created by Dirk Zimmermann on 06.09.22.
+//
+
+#import "PEPMediaKeyPair.h"
+
+@implementation PEPMediaKeyPair
+
+@end
