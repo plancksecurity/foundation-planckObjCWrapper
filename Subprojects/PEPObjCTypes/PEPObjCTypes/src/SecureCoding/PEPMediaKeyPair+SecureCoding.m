afe7429ab6173d7c198e3cb17b2d57314bb14e2a
Dirk Zimmermann <dz@pep.security>
2022-09-06 10:16:59 +0200

    PEMA-24 Start PEPMediaKeyPair <NSSecureCoding>

---
+++ Subprojects/PEPObjCTypes/PEPObjCTypes/src/SecureCoding/PEPMediaKeyPair+SecureCoding.m
@@ -0,0 +1,28 @@
+//
+//  PEPMediaKeyPair+SecureCoding.m
+//  PEPObjCTypes
+//
+//  Created by Dirk Zimmermann on 06.09.22.
+//
+
+#import "PEPMediaKeyPair+SecureCoding.h"
+
+@implementation PEPMediaKeyPair (SecureCoding)
+
+- (void)encodeWithCoder:(nonnull NSCoder *)coder
+{
+    // TODO
+}
+
+- (nullable instancetype)initWithCoder:(nonnull NSCoder *)coder
+{
+    // TODO
+    return nil;
+}
+
++ (BOOL)supportsSecureCoding
+{
+    return YES;
+}
+
+@end
