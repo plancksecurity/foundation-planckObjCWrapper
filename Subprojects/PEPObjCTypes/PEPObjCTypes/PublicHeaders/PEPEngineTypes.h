4af29a158ec6a49b24144fcf98d34fdadb38dc7c
Dirk Zimmermann <dz@pep.security>
2022-09-07 15:45:50 +0200

    PEMA-24 Whitespace

--- Subprojects/PEPObjCTypes/PEPObjCTypes/PublicHeaders/PEPEngineTypes.h
+++ Subprojects/PEPObjCTypes/PEPObjCTypes/PublicHeaders/PEPEngineTypes.h
@@ -14,7 +14,7 @@
 typedef NS_CLOSED_ENUM(int, PEPDecryptFlags) {
     PEPDecryptFlagsNone = 0x0, // not actually defined in the engine
     PEPDecryptFlagsOwnPrivateKey = 0x1, // PEP_decrypt_flag_own_private_key
-    PEPDecryptFlagsConsume = 0x2, //PEP_decrypt_flag_consume
+    PEPDecryptFlagsConsume = 0x2, // PEP_decrypt_flag_consume
     PEPDecryptFlagsIgnore = 0x4, // PEP_decrypt_flag_ignore
     PEPDecryptFlagsSourceModified = 0x8, // PEP_decrypt_flag_src_modified
     PEPDecryptFlagsUntrustedServer = 0x100, // PEP_decrypt_flag_untrusted_server
