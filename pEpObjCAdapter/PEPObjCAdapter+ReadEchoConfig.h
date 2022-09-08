2c7aa708a0cf428bb7c48cf09e8bb96ec5fdd4bd
Dirk Zimmermann <dz@pep.security>
2022-09-08 12:36:04 +0200

    PEMA-24 Add PEPObjCAdapter+ReadEchoConfig.h

---
+++ pEpObjCAdapter/PEPObjCAdapter+ReadEchoConfig.h
@@ -0,0 +1,13 @@
+//
+//  PEPObjCAdapter+ReadEchoConfig.h
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 08.09.22.
+//  Copyright © 2022 p≡p. All rights reserved.
+//
+
+#ifndef PEPObjCAdapter_ReadEchoConfig_h
+#define PEPObjCAdapter_ReadEchoConfig_h
+
+
+#endif /* PEPObjCAdapter_ReadEchoConfig_h */
