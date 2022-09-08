14d83259c5b409b23d058ec7ac688fb881dd47aa
Dirk Zimmermann <dz@pep.security>
2022-09-08 13:13:55 +0200

    PEMA-24 Start PEPObjCAdapter extension

--- pEpObjCAdapter/PEPObjCAdapter+ReadEchoConfig.h
+++ pEpObjCAdapter/PEPObjCAdapter+ReadEchoConfig.h
@@ -9,5 +9,10 @@
 #ifndef PEPObjCAdapter_ReadEchoConfig_h
 #define PEPObjCAdapter_ReadEchoConfig_h
 
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPObjCAdapter (ReadEchoConfig)
+
+@end
 
 #endif /* PEPObjCAdapter_ReadEchoConfig_h */
