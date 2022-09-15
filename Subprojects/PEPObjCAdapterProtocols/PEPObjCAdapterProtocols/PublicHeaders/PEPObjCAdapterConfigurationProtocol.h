85f9ba74c2d4b25672e71090810c0a910c3a5420
Dirk Zimmermann <dz@pep.security>
2022-09-15 10:03:13 +0200

    PEMA-43 Reflect the naming in the header file names

--- Subprojects/PEPObjCAdapterProtocols/PEPObjCAdapterProtocols/PublicHeaders/PEPObjCAdapterConfigurationProtocol.h
+++ Subprojects/PEPObjCAdapterProtocols/PEPObjCAdapterProtocols/PublicHeaders/PEPObjCAdapterConfigurationProtocol.h
@@ -8,8 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
-#import "PEPObjCAdapterConfigurationProtocol+Echo.h"
-#import "PEPObjCAdapterConfigurationProtocol+MediaKeys.h"
+#import "PEPObjCAdapterEchoConfigurationProtocol.h"
+#import "PEPObjCAdapterMediaKeysConfigurationProtocol.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
