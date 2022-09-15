8f72a7e969da7c18df89f0c30be01042ca9d6743
Dirk Zimmermann <dz@pep.security>
2022-09-15 09:59:10 +0200

    PEMA-43 Start declaring the protocol

--- Subprojects/PEPObjCAdapterProtocols/PEPObjCAdapterProtocols/PublicHeaders/PEPObjCAdapterConfigurationProtocol+MediaKeys.h
+++ Subprojects/PEPObjCAdapterProtocols/PEPObjCAdapterProtocols/PublicHeaders/PEPObjCAdapterConfigurationProtocol+MediaKeys.h
@@ -8,5 +8,13 @@
 #ifndef PEPObjCAdapterConfigurationProtocol_MediaKeys_h
 #define PEPObjCAdapterConfigurationProtocol_MediaKeys_h
 
+NS_ASSUME_NONNULL_BEGIN
+
+/// @see https://dev.pep.foundation/Engine/Media%20keys
+@protocol PEPObjCAdapterEchoConfigurationProtocol <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
 
 #endif /* PEPObjCAdapterConfigurationProtocol_MediaKeys_h */
