dac24635128d3d57b9a6a450494cf7aac6c5df2f
Dirk Zimmermann <dz@pep.security>
2022-09-15 09:57:15 +0200

    PEMA-43 PEPObjCAdapterConfigurationProtocol+MediaKeys.h

---
+++ Subprojects/PEPObjCAdapterProtocols/PEPObjCAdapterProtocols/PublicHeaders/PEPObjCAdapterConfigurationProtocol+MediaKeys.h
@@ -0,0 +1,12 @@
+//
+//  PEPObjCAdapterConfigurationProtocol+MediaKeys.h
+//  PEPObjCAdapterProtocols
+//
+//  Created by Dirk Zimmermann on 15.09.22.
+//
+
+#ifndef PEPObjCAdapterConfigurationProtocol_MediaKeys_h
+#define PEPObjCAdapterConfigurationProtocol_MediaKeys_h
+
+
+#endif /* PEPObjCAdapterConfigurationProtocol_MediaKeys_h */
