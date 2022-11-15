3a3b8266708c8e9f5660b241f230c91589afba8d
Dirk Zimmermann <dz@pep.security>
2022-11-15 14:02:38 +0100

    PEMA-41 Define IS_IOS_BUILD (or not)

---
+++ src/Platform.h
@@ -0,0 +1,25 @@
+//
+//  Platform.h
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 15.11.22.
+//  Copyright © 2022 p≡p. All rights reserved.
+//
+
+#ifndef Platform_h
+#define Platform_h
+
+#ifdef __APPLE__
+#include "TargetConditionals.h"
+#ifdef TARGET_OS_IPHONE
+#define IS_IOS_BUILD
+#elif TARGET_IPHONE_SIMULATOR
+#define IS_IOS_BUILD
+#elif TARGET_OS_MAC
+#undefine IS_IOS_BUILD
+#else
+#undefine IS_IOS_BUILD
+#endif
+#endif
+
+#endif /* Platform_h */
