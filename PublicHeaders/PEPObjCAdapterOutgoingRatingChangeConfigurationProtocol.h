b720620d07fdcfa2009a8f1cfd1509bf0cf3bbed
Dirk Zimmermann <dz@pep.security>
2022-12-01 08:56:52 +0100

    PEMA-54 PEPObjCAdapterOutgoingRatingChangeConfigurationProtocol.h

---
+++ PublicHeaders/PEPObjCAdapterOutgoingRatingChangeConfigurationProtocol.h
@@ -0,0 +1,13 @@
+//
+//  PEPObjCAdapterOutgoingRatingChangeConfigurationProtocol.h
+//  pEpObjCAdapter
+//
+//  Created by Dirk Zimmermann on 01.12.22.
+//  Copyright © 2022 p≡p. All rights reserved.
+//
+
+#ifndef PEPObjCAdapterOutgoingRatingChangeConfigurationProtocol_h
+#define PEPObjCAdapterOutgoingRatingChangeConfigurationProtocol_h
+
+
+#endif /* PEPObjCAdapterOutgoingRatingChangeConfigurationProtocol_h */
