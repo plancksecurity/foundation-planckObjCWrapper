d1ddcb78195ff38d5e3a55ea3bcfc5f6be9a0eb4
Dirk Zimmermann <dz@pep.security>
2023-01-11 12:29:43 +0100

    PEMA-158 Add more enum values, when defined

--- Subprojects/PEPObjCTypes/PublicHeaders/PEPEngineTypes.h
+++ Subprojects/PEPObjCTypes/PublicHeaders/PEPEngineTypes.h
@@ -52,7 +52,7 @@ typedef NS_CLOSED_ENUM(int, PEPSyncHandshakeSignal) {
 typedef NS_CLOSED_ENUM(int, PEPContentDisposition) {
     PEPContentDispositionAttachment = 0, // PEP_CONTENT_DISP_ATTACHMENT
     PEPContentDispositionInline = 1, // PEP_CONTENT_DISP_INLINE
-    PEPContentDispositionOther, // PEP_CONTENT_DISP_OTHER
+    PEPContentDispositionOther = -1, // PEP_CONTENT_DISP_OTHER
 };
 
 typedef NS_CLOSED_ENUM(int, PEPMsgDirection) {
@@ -125,47 +125,47 @@ typedef NS_CLOSED_ENUM(int, PEPEncryptFlags) {
 };
 
 typedef NS_CLOSED_ENUM(int, PEPIdentityFlags) {
-    PEPIdentityFlagsNotForSync, // PEP_idf_not_for_sync
-    PEPIdentityFlagsList, // PEP_idf_list
-    PEPIdentityFlagsDevicegroup, // PEP_idf_devicegroup
+    PEPIdentityFlagsNotForSync = 0x0001, // PEP_idf_not_for_sync
+    PEPIdentityFlagsList = 0x0002, // PEP_idf_list
+    PEPIdentityFlagsDevicegroup = 0x0100, // PEP_idf_devicegroup
 };
 
 typedef NS_CLOSED_ENUM(int, PEPCommType) {
     PEPCommTypeUnknown = 0, // PEP_ct_unknown
-    PEPCommTypeNoEncryption, // PEP_ct_no_encryption
+    PEPCommTypeNoEncryption = 0x01, // PEP_ct_no_encryption
     PEPCommTypeNoEncryptedChannel = 0x02, // PEP_ct_no_encrypted_channel
     PEPCommTypeKeyNotFound = 0x03, // PEP_ct_key_not_found
     PEPCommTypeKeyExpired = 0x04, // PEP_ct_key_expired
     PEPCommTypeKeyRevoked = 0x05, // PEP_ct_key_revoked
     PEPCommTypeKeyB0rken = 0x06, // PEP_ct_key_b0rken
-    PEPCommTypeKeyExpiredButConfirmed, // PEP_ct_key_expired_but_confirmed
+    PEPCommTypeKeyExpiredButConfirmed = 0x07, // PEP_ct_key_expired_but_confirmed
     PEPCommTypeMyKeyNotIncluded = 0x09, // PEP_ct_my_key_not_included
     PEPCommTypeSecurityByObscurity = 0x0a, // PEP_ct_security_by_obscurity
     PEPCommTypeB0rkenCrypto = 0x0b, // PEP_ct_b0rken_crypto
     PEPCommTypeKeyTooShort = 0x0c, // PEP_ct_key_too_short
-    PEPCommTypeCompromised, // PEP_ct_compromised
-    PEPCommTypeCompromized, // PEP_ct_compromized
-    PEPCommTypeMistrusted, // PEP_ct_mistrusted
-    PEPCommTypeUnconfirmedEncryption, // PEP_ct_unconfirmed_encryption
-    PEPCommTypeOpenPGPWeakUnconfirmed, // PEP_ct_OpenPGP_weak_unconfirmed
-    PEPCommTypeToBeChecked, // PEP_ct_to_be_checked
+    PEPCommTypeCompromised = 0x0e, // PEP_ct_compromised
+    PEPCommTypeCompromized = 0x0e, // PEP_ct_compromized
+    PEPCommTypeMistrusted = 0x0f, // PEP_ct_mistrusted
+    PEPCommTypeUnconfirmedEncryption = 0x10, // PEP_ct_unconfirmed_encryption
+    PEPCommTypeOpenPGPWeakUnconfirmed = 0x11, // PEP_ct_OpenPGP_weak_unconfirmed
+    PEPCommTypeToBeChecked = 0x20, // PEP_ct_to_be_checked
     PEPCommTypeSmimeUnconfirmed = 0x21, // PEP_ct_SMIME_unconfirmed
     PEPCommTypeCmsUnconfirmed = 0x22, // PEP_ct_CMS_unconfirmed
-    PEPCommTypeStrongButUnconfirmed, // PEP_ct_strong_but_unconfirmed
-    PEPCommTypeOpenPGPUnconfirmed, // PEP_ct_OpenPGP_unconfirmed
+    PEPCommTypeStrongButUnconfirmed = 0x30, // PEP_ct_strong_but_unconfirmed
+    PEPCommTypeOpenPGPUnconfirmed = 0x38, // PEP_ct_OpenPGP_unconfirmed
     PEPCommTypeOtrUnconfirmed = 0x3a, // PEP_ct_OTR_unconfirmed
-    PEPCommTypeUnconfirmedEncAnon, // PEP_ct_unconfirmed_enc_anon
+    PEPCommTypeUnconfirmedEncAnon = 0x40, // PEP_ct_unconfirmed_enc_anon
     PEPCommTypePEPUnconfirmed = 0x7f, // PEP_ct_pEp_unconfirmed
-    PEPCommTypeConfirmed, // PEP_ct_confirmed
-    PEPCommTypeConfirmedEncryption, // PEP_ct_confirmed_encryption
-    PEPCommTypeOpenPGPWeak, // PEP_ct_OpenPGP_weak
-    PEPCommTypeToBeCheckedConfirmed, // PEP_ct_to_be_checked_confirmed
+    PEPCommTypeConfirmed = 0x80, // PEP_ct_confirmed
+    PEPCommTypeConfirmedEncryption = 0x90, // PEP_ct_confirmed_encryption
+    PEPCommTypeOpenPGPWeak = 0x91, // PEP_ct_OpenPGP_weak
+    PEPCommTypeToBeCheckedConfirmed = 0xa0, // PEP_ct_to_be_checked_confirmed
     PEPCommTypeSmime = 0xa1, // PEP_ct_SMIME
     PEPCommTypeCms = 0xa2, // PEP_ct_CMS
-    PEPCommTypeStrongEncryption, // PEP_ct_strong_encryption
-    PEPCommTypeOpenPGP, // PEP_ct_OpenPGP
+    PEPCommTypeStrongEncryption = 0xb0, // PEP_ct_strong_encryption
+    PEPCommTypeOpenPGP = 0xb8, // PEP_ct_OpenPGP
     PEPCommTypeOtr = 0xba, // PEP_ct_OTR
-    PEPCommTypeConfirmedEncAnon, // PEP_ct_confirmed_enc_anon
+    PEPCommTypeConfirmedEncAnon = 0xc0, // PEP_ct_confirmed_enc_anon
     PEPCommTypePEP = 0xff, // PEP_ct_pEp
 };
 
@@ -182,15 +182,15 @@ typedef NS_CLOSED_ENUM(int, PEPCipherSuite) {
 };
 
 typedef NS_CLOSED_ENUM(int, PEPEncFormat) {
-    PEPEncFormatNone, // PEP_enc_none
-    PEPEncFormatPieces, // PEP_enc_pieces
-    PEPEncFormatInline, // PEP_enc_inline
+    PEPEncFormatNone = 0, // PEP_enc_none
+    PEPEncFormatPieces = 1, // PEP_enc_pieces
+    PEPEncFormatInline = 1, // PEP_enc_inline
     PEPEncFormatSMime, // PEP_enc_S_MIME
     PEPEncFormatPGPMime, // PEP_enc_PGP_MIME
     PEPEncFormatPEP, // PEP_enc_PEP
     PEPEncFormatPGPMimeOutlook1, // PEP_enc_PGP_MIME_Outlook1
     PEPEncFormatInlineEa, // PEP_enc_inline_EA
-    PEPEncFormatAuto, // PEP_enc_auto
+    PEPEncFormatAuto = 255, // PEP_enc_auto
 };
 
 typedef NS_CLOSED_ENUM(int, PEPStatus) {
