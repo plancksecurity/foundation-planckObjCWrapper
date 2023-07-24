bfaa3ff725ba9ecb058ca9422dc26ecc9df94773
Dirk Zimmermann <dz@planck.security>
2023-07-24 16:23:57 +0200

    CORE-61 Start NSString FPR extension

---
+++ Subprojects/PEPObjCTypes/src/Extensions/NSString+FPR.h
@@ -0,0 +1,16 @@
+//
+//  NSString+FPR.h
+//  PEPObjCTypes
+//
+//  Created by Dirk Zimmermann on 24/7/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (FPR)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Subprojects/PEPObjCTypes/src/Extensions/NSString+FPR.m
@@ -0,0 +1,12 @@
+//
+//  NSString+FPR.m
+//  PEPObjCTypes
+//
+//  Created by Dirk Zimmermann on 24/7/23.
+//
+
+#import "NSString+FPR.h"
+
+@implementation NSString (FPR)
+
+@end
